cda5c7b11a03a2bd7e66eb37c57ba691ec961f4f
S <s>
2023-09-25 20:11:56 +0400

    {"types":"C","runs":1}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -11,7 +11,7 @@ public class ConsoleProgrammSample extends ConsoleProgram {
 
 	public void run() {
 		// You fill this in
-		printLine("Hello World!");
+		System.out.print("Hello World!");
 	}
 
 }
