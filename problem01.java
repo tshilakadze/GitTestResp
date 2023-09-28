eca4213017c7db89ccf3a2262f49f47fdebf6e94
S <s>
2023-09-28 23:01:10 +0400

    {"types":"C","runs":1}

--- problem01.java
+++ problem01.java
@@ -1,8 +1,8 @@
 import stanford.karel.Karel;
 
 public class problem01 extends Karel{
-/* კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
-/* გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
+// კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
+// გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
 	public void run(){
 		while(noBeepersPresent()){move();}
 		pickBeeper();
