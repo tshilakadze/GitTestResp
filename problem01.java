71991916071d0fa3f1bc4ac571a9f9805c518e7d
S <s>
2023-09-28 17:45:21 +0400

    {"types":"C","runs":14}

--- problem01.java
+++ problem01.java
@@ -4,6 +4,6 @@ public class problem01 extends Karel{
 // კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
 // გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
 	public void run(){
-		while(noBeepersPresent()){move()}
+		while(noBeepersPresent()){move();}
 	}
 }
