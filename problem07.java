9d5ce49ae2e3f6a088a283cfacd45de9c07a6e54
S <s>
2023-09-29 15:35:16 +0400

    {"types":"C","runs":46}

--- problem07.java
+++ problem07.java
@@ -3,6 +3,9 @@ public class problem07 extends SuperKarel {
 	/* Karel places 1 beeper on every empty space
 	 */
 	public void run(){
+		fillLine();}
+
+	private void fillLine(){
 		while(frontIsClear()){
 			if(noBeepersPresent()){putBeeper();
 			move();
@@ -10,32 +13,6 @@ public class problem07 extends SuperKarel {
 			else{move();}
 		}
 		if(noBeepersPresent()){
-			putBeeper();}
-		if(leftIsClear()){
-			turnLeft();
-			move();
-			turnLeft();
-			while(frontIsClear()){
-				if(noBeepersPresent()){putBeeper();
-				move();
-				}
-				else{move();}
-			}
-			if(noBeepersPresent()){
-				putBeeper();}
-		}
-		if(leftIsClear()){
-			turnRight();
-			move();
-			turnLeft();
-			while(frontIsClear()){
-				if(noBeepersPresent()){putBeeper();
-				move();
-				}
-				else{move();}
-			}
-			if(noBeepersPresent()){
-				putBeeper();}
-		}
+		putBeeper();}
 	}
 }
