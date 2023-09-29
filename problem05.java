15cf44ad9600be13acaafc39b7bc85e3e2fdc901
S <s>
2023-09-29 13:02:06 +0400

    {"types":"C","runs":8}

--- problem05.java
+++ problem05.java
@@ -1,7 +1,12 @@
 import stanford.karel.Karel;
 public class problem05 extends Karel {
 	public void run(){
-		
+		while(frontIsClear()){
+			if(noBeepersPresent()){putBeeper();
+			move();
+			}
+			else{move();}
+		}
 	}
 
 }
