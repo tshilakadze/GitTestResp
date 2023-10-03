dd0b9174ad66d1fb8f4899c51ee0c5c4bcab16fd
S <s>
2023-10-03 17:15:04 +0400

    {"types":"C","runs":4}

--- seminari_3_sakhlebi.java
+++ seminari_3_sakhlebi.java
@@ -1,4 +1,14 @@
 import stanford.karel.SuperKarel;
 public class seminari_3_sakhlebi extends SuperKarel{
-	
+	public void run(){
+		move();
+		move();
+		paintHouse();
+	}
+	private void paintHouse(){
+		turnLeft();
+		for(int i=0; i<9; i++){
+			putBeeper();
+		}
+	}
 }
