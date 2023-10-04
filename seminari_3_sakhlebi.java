095723d3e1b3f9a3b6ae51fa8352a6aae85c6896
S <s>
2023-10-04 23:43:46 +0400

    {"types":"C","runs":17}

--- seminari_3_sakhlebi.java
+++ seminari_3_sakhlebi.java
@@ -7,9 +7,6 @@ public class seminari_3_sakhlebi extends SuperKarel{
 		drawHouse();
 		}
 		returnHome();
-		for(int i=0; i<4; i++){
-		move();}
-		turnLeft();
 		for(int i=0; i<5; i++){
 		drawDoor();}
 	}
