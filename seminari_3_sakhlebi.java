99867a5bdf171efbc995de68119a76314d87a4a8
S <s>
2023-10-04 23:43:14 +0400

    {"types":"C","runs":16}

--- seminari_3_sakhlebi.java
+++ seminari_3_sakhlebi.java
@@ -43,11 +43,26 @@ public class seminari_3_sakhlebi extends SuperKarel{
 		turnAround();
 	}
 	private void drawDoor(){
+		for(int i=0; i<4; i++){
+			move();
+		}
+		turnLeft();
 		putBeeper();
 		for(int i=0; i<3; i++){
 			move();
 			putBeeper();
 		}
+		turnRight();
+		move();
+		putBeeper();
+		move();
+		putBeeper();
+		turnRight();
+		for(int i=0; i<3; i++){
+			move();
+			putBeeper();
+		}
 	}
+
 	
 }
