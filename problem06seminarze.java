7a1435b7148480c224d60912c6b8ac6405d2d932
S <s>
2023-10-05 17:22:25 +0400

    {"types":"C","runs":0}

--- problem06seminarze.java
+++ problem06seminarze.java
@@ -1,5 +1,30 @@
+import stanford.karel.SuperKarel;
+public class problem06seminarze extends SuperKarel{
+	public void run(){
+		move();
+		moveFirstPileToDestinationAndBackup();
+		move();
+		moveSecondPileToDestinationAndBackup();
+		restoreFromBackup();
+		restoreFromBackup();
+	}
 
+	private void moveFirstPileToDestinationAndBackup() {
+		while(beepersPresent()){
+			pickBeeper();
+			moveBeeperFromFirstPileToDestination();
+			backUpBeeper()
+		}
+	}
 
-public class problem06seminarze {
-
+	private void moveBeeperFromFirstPileToDestination() {
+		move();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		move();
+		turnAround();
+	}
+	
 }
