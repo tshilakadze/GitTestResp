ad1d8b0cb06241792fe213c1948cf6783a59dfc0
S <s>
2023-10-08 16:19:07 +0400

    {"types":"C","runs":4}

--- problem09.java
+++ problem09.java
@@ -1,6 +1,24 @@
 import stanford.karel.SuperKarel;
 public class problem09 extends SuperKarel {
+	/*
+	 * Karel multiplies the number of beepers on 2x1 and on 3x1 and puts the product on 4x1
+	 */
 	public void run(){
-		
+		move();
+		multiplyBeepers();
+	}
+
+	private void multiplyBeepers() {
+		while(beepersPresent()){
+			pickBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			turnRight();
+			move();
+			turnRight();
+			move();
+			turnLeft();
+		}
 	}
 }
