4329fbcd7d0b28e1c17dfa1ede1bd5e4543aaa14
S <s>
2023-10-10 17:44:48 +0400

    {"types":"C","runs":13}

--- seminari101023.java
+++ seminari101023.java
@@ -7,6 +7,7 @@ public class seminari101023 extends GraphicsProgram{
 	public void run() {
 		GOval myOval = new GOval(100, 200);
 		add(myOval, 100, 100);
+		myOval.setFilled(true);
 		myOval.setColor(Color.RED);
 		
 		GOval secondOval = new GOval(100, 200);
