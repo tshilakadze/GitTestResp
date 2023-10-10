8a2916793e8b8280ff4f6a66daef9598efb55bf4
S <s>
2023-10-10 17:27:23 +0400

    {"types":"C","runs":6}

--- seminari101023.java
+++ seminari101023.java
@@ -4,6 +4,6 @@ import acm.program.GraphicsProgram;
 public class seminari101023 extends GraphicsProgram{
 	public void run() {
 		GOval myOval = new GOval(100, 100);
-		add(myOval (200, 200), 100, 100);
+		add(myOval, 100, 100);
 	}
 }
