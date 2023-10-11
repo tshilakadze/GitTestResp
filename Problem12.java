f35c92c38cb938f8205aed75241e4866b6ae7bdd
S <s>
2023-10-11 23:43:38 +0400

    {"types":"C","runs":0}

--- Problem12.java
+++ Problem12.java
@@ -4,5 +4,12 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 public class Problem12 extends GraphicsProgram{
 	public void run(){
+		drawAHouse();
+		
+	}
+
+	private void drawAHouse() {
+		GRect myHouse = new GRect(100, 200);
+		add(myHouse, 200, 200);
 	}
 }
