13665a095c0a4a6ee77d866f79555e4babe8c6e9
S <s>
2023-10-11 23:53:53 +0400

    {"types":"C","runs":8}

--- Problem12.java
+++ Problem12.java
@@ -16,7 +16,7 @@ public class Problem12 extends GraphicsProgram{
 	}
 	
 	private void drawWindow() {
-		GRect aWindow = new GRect(150, 150);
+		GRect aWindow = new GRect(150, 300);
 		add(aWindow, 400, 300);
 	}
 }
