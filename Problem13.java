61407da4cc0662ee10b74db873fcd16a05309466
S <s>
2023-10-12 17:24:58 +0400

    {"types":"C","runs":5}

--- Problem13.java
+++ Problem13.java
@@ -5,7 +5,11 @@ public class Problem13 extends GraphicsProgram{
 		drawLineInTheMiddle();
 	}
 	private void drawLineInTheMiddle(){
-		GLine middleLine = new GLine(0, 600, 3000, 600);
-		add(middleLine);
+		double x1 = 0;
+		double y1 = getHeight() / 2;
+		double x2 = getWidth();
+		double y2 = y1;
+		GLine Line = new GLine(x1, y1, x2, y2);
+		add(Line);
 	}
 }
