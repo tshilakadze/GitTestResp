f2ee275b42e5fdd9ac34fba2e20bcda012d2bbe2
S <s>
2023-10-16 20:48:10 +0400

    {"types":"C","runs":30}

--- Problem14.java
+++ Problem14.java
@@ -27,7 +27,7 @@ public class Problem14 extends GraphicsProgram {
 		double x = 40;
 		double y = 50;
 		GRect Nose = new GRect(x, y);
-		add(Nose, a/2 - x/2, b/2 - y);
+		add(Nose, a/2 - x/2, b/2 - y/4);
 	}
 
 	private void drawMouth() {
