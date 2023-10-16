c16e123fe02d6995981222298249b57a6bc5ea8a
S <s>
2023-10-16 21:11:25 +0400

    {"types":"C","runs":47}

--- Problem14.java
+++ Problem14.java
@@ -45,7 +45,7 @@ public class Problem14 extends GraphicsProgram {
 		double x = 30;
 		double y = 50;
 		GRect Eye1 = new GRect(x, y);
-		add(Eye1, a/2 - x*2, b/2 - y*2);
+		add(Eye1, a/2 - x*3, b/2 - y*2);
 	}
 
 }
