ea0292f6584ae2c5484050051505c687ecb06f21
S <s>
2023-10-16 22:56:49 +0400

    {"types":"C","runs":77}

--- Problem17.java
+++ Problem17.java
@@ -3,16 +3,7 @@ import acm.program.ConsoleProgram;
 public class Problem17 extends ConsoleProgram{
 	public void run() {
 		double a = readDouble("Enter a: ");
-		if(a < 1){
-			println("Integer part of the number is 0");
-			println("Fraction of the number is " + a + ".");}
-			else{
-				double b = a - 1;
-				while(b < 1){
-					println("Fraction of the number is " + a + ".");	
-					
-				}
-			}
+		
 		
 	}
 }
