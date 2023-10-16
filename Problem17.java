17839b4410694ddf5f8499a57f243744d2bb8775
S <s>
2023-10-16 23:38:59 +0400

    {"types":"C","runs":93}

--- Problem17.java
+++ Problem17.java
@@ -5,9 +5,9 @@ public class Problem17 extends ConsoleProgram{
 		double a = readDouble("Enter a: ");
 		for(double b = 0; b < a || b==a; b++){
 			if(b+1>a){
-				println(b);	
+				println("Integer number is " + b +".");	
 				double c = a -b;
-				println(c);
+				println("Fraction is " + c + ".");
 			}
 			
 			
