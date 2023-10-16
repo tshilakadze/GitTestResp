d63259552368fee9fa395182dc14b14368f7f83a
S <s>
2023-10-16 23:36:49 +0400

    {"types":"C","runs":89}

--- Problem17.java
+++ Problem17.java
@@ -3,12 +3,13 @@ import acm.program.ConsoleProgram;
 public class Problem17 extends ConsoleProgram{
 	public void run() {
 		double a = readDouble("Enter a: ");
-		for(double b = 0; b < a; b++){
-			if(b+1>=a){
+		for(double b = 0; b < a || b==a; b++){
+			if(b+1>a){
 				println(b);	
 				double c = a -b;
 				println(c);
 			}
+			
 			
 		}
 		
