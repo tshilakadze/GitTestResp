5ae57fb4145a1378b8c8ccb2fe47054f1894d4f5
S <s>
2023-10-16 19:21:14 +0400

    {"types":"C","runs":1}

--- TestConsole01.java
+++ TestConsole01.java
@@ -1,9 +1,5 @@
 import acm.program.ConsoleProgram;
 
 public class TestConsole01 extends ConsoleProgram {
-	public void run(){
-		int kids = readInt();
-		
-		println("Your kids number: " + kids);
-	}
+	
 }
