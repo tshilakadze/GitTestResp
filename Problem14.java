c679a352e19147181a1fcae903df4df517c31b88
S <s>
2023-10-16 20:35:31 +0400

    {"types":"C","runs":18}

--- Problem14.java
+++ Problem14.java
@@ -15,8 +15,10 @@ public class Problem14 extends GraphicsProgram {
 	private void drawHead() {
 		double a = getWidth();
 		double b = getHeight();
-		GRect Head = new GRect(200, 200);
-		add(Head, a/2 - 100, b/2 - 100);
+		double c = 200;		// Face width
+		double d = 200;		// Face height
+		GRect Head = new GRect(c, d);
+		add(Head, a/2 - c/2, b/2 - d/2);
 	}
 
 	private void drawNose() {
