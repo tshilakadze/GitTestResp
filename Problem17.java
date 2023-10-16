abb2cc6d140f9746723a234e9f65c1987afa3d15
S <s>
2023-10-16 23:34:12 +0400

    {"types":"C","runs":87}

--- Problem17.java
+++ Problem17.java
@@ -4,7 +4,7 @@ public class Problem17 extends ConsoleProgram{
 	public void run() {
 		double a = readDouble("Enter a: ");
 		for(double b = 0; b < a; b++){
-			if(b+1>a){
+			if(b+1>=a){
 				println(b);	
 				double c = a -b;
 				println(c);
