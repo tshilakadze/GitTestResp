a7b79b82b05e638eaadcdab7d035af13e7a738b3
S <s>
2023-10-16 23:42:04 +0400

    {"types":"C","runs":97}

--- Problem17.java
+++ Problem17.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Problem17 extends ConsoleProgram{
 	public void run() {
 		double a = readDouble("Enter a: ");
-		if(a > 0){
+		if(a >=0){
 		for(double b = 0; b < a || b==a; b++){
 			if(b+1>a){
 				println("Integer number is " + b +".");	
