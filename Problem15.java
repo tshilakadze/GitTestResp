3ef9c62d6b858ebf821832ffd9872946dcc89a3e
S <s>
2023-10-16 22:10:40 +0400

    {"types":"C","runs":72}

--- Problem15.java
+++ Problem15.java
@@ -5,8 +5,8 @@
 import acm.program.ConsoleProgram;
 public class Problem15 extends ConsoleProgram{
 	public void run() {
-		double a = readInt("Enter a: ");
-		double b = readInt("Enter b: ");
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
 		double average = (a + b) / 2;
 		println("The arithmetic average is " + average + ".");
 	}
