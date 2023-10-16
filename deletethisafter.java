4bff1181f48786cd48174bf03a15510f55680c50
S <s>
2023-10-16 23:46:22 +0400

    {"types":"C","runs":101}

--- deletethisafter.java
+++ deletethisafter.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class deletethisafter {
-
+public class deletethisafter extends ConsoleProgram {
+	public void run(){
+	double a = readDouble("enter a: ");
+	if(a<0){
+		println("a is negative");
+	}
+	}
 }
