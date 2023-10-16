c5c74229d1776ebe49ee8d5a902ccf917f15a79a
S <s>
2023-10-16 14:14:20 +0400

    {"types":"C","runs":5}

--- HelloProgram.java
+++ HelloProgram.java
@@ -3,7 +3,6 @@ import acm.program.*;
 public class HelloProgram extends GraphicsProgram {
 	public void run() {
 		GLabel msg = new GLabel("Hello World", 100, 75);
-		msg.setFont("Lucida Blackletter-28");
 		add (msg);
 	}
 
