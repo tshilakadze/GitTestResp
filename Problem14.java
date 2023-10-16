687b680c1c5c9d3ad33b0861e6f729f8530dfcd4
S <s>
2023-10-16 20:21:02 +0400

    {"types":"C","runs":1}

--- Problem14.java
+++ Problem14.java
@@ -2,6 +2,33 @@ import acm.program.*;
 import acm.graphics.*;
 public class Problem14 extends GraphicsProgram {
 	public void run() {
+		drawRobot();
+	}
+
+	private void drawRobot() {
+		drawHead();
+		drawNose();
+		drawMouth();
+		drawEyes();
+	}
+
+	private void drawHead() {
+		GRect Head = new GRect(200, 200);
+		add(Head);
+	}
+
+	private void drawNose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawMouth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawEyes() {
+		// TODO Auto-generated method stub
 		
 	}
 
