baeba084ec32647208044ca3d3f831db449ca285
S <s>
2023-10-16 21:35:20 +0400

    {"types":"C","runs":57}

--- Problem15.java
+++ Problem15.java
@@ -1,4 +1,14 @@
+/*
+ * Program "Problem15" calculates arithmetic average of two integers
+ */
 
-public class Problem15 {
-
+import acm.program.ConsoleProgram;
+import acm.program.*;
+public class Problem15 extends ConsoleProgram{
+	public void run() {
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int average = (a + b) / 2;
+		println("The arithmetic average is "+ average + ".");
+	}
 }
