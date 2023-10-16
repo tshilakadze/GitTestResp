2ccca59068b92f169ba1ff160249c4d467751ab1
S <s>
2023-10-16 14:09:57 +0400

    {"types":"C","runs":0}

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,4 +1,9 @@
-
-public class HelloProgram {
+import acm.graphics.*;
+import acm.program.*;
+public class HelloProgram extends GraphicsProgram {
+	public void run() {
+		GLabel msg = new GLabel("Hello World", 100, 75);
+		add (msg);
+	}
 
 }
