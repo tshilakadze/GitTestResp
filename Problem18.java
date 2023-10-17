9ad43982d6707536171ccdd5d12e6ff8d5299d15
S <s>
2023-10-17 16:00:54 +0400

    {"types":"C","runs":0}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem18 {
+public class Problem18 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int c = a - b;
+		if(c > 0){
+			println("The biggest number is " + a + ".");
+		}
+		else{
+			println("The biggest number is " + b + ".");
+		}
+	}
 
 }
