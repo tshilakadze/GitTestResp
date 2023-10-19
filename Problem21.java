0b0f12554e20f8b1e291c4c312e6b738b049101f
S <s>
2023-10-19 17:39:08 +0400

    {"types":"C","runs":7}

--- Problem21.java
+++ Problem21.java
@@ -7,7 +7,7 @@ public class Problem21 extends GraphicsProgram{
 			GLine line = new GLine(m, 0, m, getHeight());
 			add(line);
 		}
-		for(int n = 0; n < getHeight(); n = n + getHeight()/10){
+		for(int n = 0; n < getHeight(); n = n + getHeight()/11){
 			GLine HorLine = new GLine(0, n, getWidth(), n);
 			add(HorLine);
 		}
