25e254dfd796ba835569df97360cd386c6d6ccb5
S <s>
2023-10-19 17:16:11 +0400

    {"types":"C","runs":4}

--- Problem22.java
+++ Problem22.java
@@ -3,6 +3,7 @@ import acm.program.GraphicsProgram;
 public class Problem22 extends GraphicsProgram {
 	public void run() {
 		double sqWidth = getWidth()/8;
+		double sqHeight = getHeight()/8;
 		
 	}
 }
