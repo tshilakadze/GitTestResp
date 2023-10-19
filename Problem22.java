313ce966bba486e6c03d41603888f5d7dbb64c73
S <s>
2023-10-19 17:40:04 +0400

    {"types":"C","runs":8}

--- Problem22.java
+++ Problem22.java
@@ -6,7 +6,7 @@ public class Problem22 extends GraphicsProgram {
 		double sqHeight = getHeight()/8;
 		GRect sq = new GRect(sqWidth, sqHeight);
 		for(int i = 0; i < getWidth() - getWidth()/8; i = i + getWidth()/8){
-			
+			add(sq, i, 0);
 		}
 	}
 }
