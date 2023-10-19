bddf42d4891f61113b364e1be55809318a32065d
S <s>
2023-10-19 16:38:29 +0400

    {"types":"C","runs":12}

--- testproject.java
+++ testproject.java
@@ -52,3 +52,17 @@ public class testproject extends GraphicsProgram{
 			add(Line);
 		}
 }
+
+
+
+
+double a = getWidth()/10;
+GLine line = new GLine(a, 0, a, getHeight());
+add(line);
+
+for(int i = 0; i < 9; i++){
+double b = a + getWidth()/10;
+GLine OtherLines = new GLine(b, 0, b, getHeight());
+add(OtherLines);
+
+}
