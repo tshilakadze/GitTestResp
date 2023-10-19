916ebafe529c370c7e42db396a60558c8c976f8b
S <s>
2023-10-19 16:38:22 +0400

    {"types":"C","runs":11}

--- Problem21.java
+++ Problem21.java
@@ -3,40 +3,34 @@ import acm.program.GraphicsProgram;
 
 public class Problem21 extends GraphicsProgram{
 	public void run(){
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		double a = getWidth()/10;
-		GLine line = new GLine(a, 0, a, getHeight());
-		add(line);
-		
-		for(int i = 0; i < 9; i++){
-		double b = a + getWidth()/10;
-		GLine OtherLines = new GLine(b, 0, b, getHeight());
-		add(OtherLines);
-		
+		for(int m = 0; m < getWidth(); m = m + getWidth()/10){
+			GLine line = new GLine(m, 0, m, getHeight());
+			add(line);
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
