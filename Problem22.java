aad0649a2a062dc7276e4f55d34842fea88a1bae
S <s>
2023-10-19 17:11:53 +0400

    {"types":"C","runs":3}

--- Problem22.java
+++ Problem22.java
@@ -1,4 +1,8 @@
+import acm.program.GraphicsProgram;
 
-public class Problem22 {
-
+public class Problem22 extends GraphicsProgram {
+	public void run() {
+		double sqWidth = getWidth()/8;
+		
+	}
 }
