be22a97bb2efaab786cbd1e5707f929792ee5fcb
S <s>
2023-10-19 16:18:22 +0400

    {"types":"C","runs":4}

--- Problem21.java
+++ Problem21.java
@@ -4,6 +4,7 @@ import acm.program.GraphicsProgram;
 public class Problem21 extends GraphicsProgram{
 	public void run(){
 		GLine line = new GLine(getWidth()/10, 0, getWidth()/10, getHeight());
+		add(line);
 	}
 
 }
