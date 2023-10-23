3ad802f91dab6f197d7b1c9ab1f5d3c053dc2754
S <s>
2023-10-23 19:19:13 +0400

    {"types":"C","runs":60}

--- Problem25.java
+++ Problem25.java
@@ -6,14 +6,14 @@ public class Problem25 extends ConsoleProgram {
 		
 		int m = readInt("Enter the first number: ");
 		int n = readInt("Enter the second number: ");
-		int HCF = 0;		// highest common factor
+		int a = 0;		// highest common factor
 		for(int i = 1; i <= m && i <= n; i++){
 			if(m % i == 0 && n % i == 0){
-				HCF = i;
+				a = i;
 			}
 			
 		}
-		println("Hieghest common factor is "+ HCF);
+		println(a);
 		
 	}
 }
