255b1279563b58635bce199c446b54babc950f6c
S <s>
2023-10-23 19:22:22 +0400

    {"types":"C","runs":62}

--- Problem26.java
+++ Problem26.java
@@ -1,16 +1,19 @@
 import acm.program.ConsoleProgram;
 
 public class Problem26 extends ConsoleProgram {
+	
 	public void run() {
+		
 		int m = readInt("Enter the first number: ");
 		int n = readInt("Enter the second number: ");
-		int a = Math.min(m, n);
-		int b = Math.max(m, n);
-		for(int i = a + 1; i <= b; i++){
-			if(b % i == 0 && i % a != 0){
-				a *= i;
+		int a = 0;		// highest common factor
+		for(int i = 1; i <= m && i <= n; i++){
+			if(m % i == 0 && n % i == 0){
+				a = i;
 			}
+			
 		}
 		println(a);
+		
 	}
 }
