68e47361556d504b12d83af6037026f664edd0ab
S <s>
2023-10-23 19:10:10 +0400

    {"types":"C","runs":47}

--- Problem26.java
+++ Problem26.java
@@ -4,7 +4,13 @@ public class Problem26 extends ConsoleProgram {
 	public void run() {
 		int m = readInt("Enter the first number: ");
 		int n = readInt("Enter the second number: ");
-		int a  = Math.min(m, n);
+		int a = Math.min(m, n);
+		int b = Math.max(m, n);
+		for(int i = a + 1; i <= b; i++){
+			if(b % i == 0){
+				a *= i;
+			}
+		}
 		println(a);
 	}
 }
