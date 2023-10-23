0362d33210b118bf34b2c5492af66d052513dfd1
S <s>
2023-10-23 18:22:15 +0400

    {"types":"C","runs":29}

--- Problem26.java
+++ Problem26.java
@@ -2,7 +2,14 @@ import acm.program.ConsoleProgram;
 
 public class Problem26 extends ConsoleProgram {
 	public void run() {
-		
+		int m = readInt("Enter the first number: ");
+		int n = readInt("Enter the second number: ");
+		int LCM = 1;
+		for(int i = 1; i <= m || i <= n; i++){
+			if(m % i == 0 || n % i == 0){
+				LCM *= i;
+			}
 	}
-
+		println("Lowest common multiple is "+ LCM);
+	}
 }
