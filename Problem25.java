19fc023a02b529eed1c7c9a9eaf96f5f553d274d
S <s>
2023-10-23 18:08:34 +0400

    {"types":"C","runs":20}

--- Problem25.java
+++ Problem25.java
@@ -4,7 +4,16 @@ public class Problem25 extends ConsoleProgram {
 	
 	public void run() {
 		
-		
+		int m = readInt("Enter the first number: ");
+		int n = readInt("Enter the second number: ");
+		int a = 1;
+		for(int i = 1; i <= m && i <= n; i++){
+			if(m % i == 0 && n % i == 0){
+				a *=i;
+			}
+			
+		}
+		println(a);
 		
 	}
 }
