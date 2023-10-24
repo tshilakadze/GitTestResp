7a3682dc793ed77676e45102602132082fcabf39
S <s>
2023-10-24 17:49:05 +0400

    {"types":"C","runs":6}

--- Problem25.java
+++ Problem25.java
@@ -4,10 +4,6 @@ public class Problem25 extends ConsoleProgram {
 	public void run() {
 		int m = readInt("Enter the first number: ");
 		int n = readInt("Enter the second number: ");
-		while(n < 0 || m < 0){
-			println("Numbers mustn't be negative.");
-			
-		}
 		int a = 0;
 		for(int i = 1; i <= m || i <= n; i++){
 			if(m % i == 0 && n % i == 0){
