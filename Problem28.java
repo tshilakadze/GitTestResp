7489038bd72f3b41061625e2365ef138005601be
S <s>
2023-10-26 00:22:47 +0400

    {"types":"C","runs":32}

--- Problem28.java
+++ Problem28.java
@@ -2,27 +2,29 @@ import acm.program.ConsoleProgram;
 
 public class Problem28 extends ConsoleProgram {
 	public void run() {
-		int n = readPositiveInts("Enter a number ");
 		
+		int n = readPositiveInts("Enter a number ");
 		
 	}
 
 	private int readPositiveInts(String string) {
-		int NOE = 0;		// Number Of Even numbers
 		int n = readInt(string);
 		if(n >= 0){
 			while(n >= 0){
 				int m = readInt(string);
 				
 				if(m < 0){
-					
+					if(m >= 0 && m % 2 == 0){
+						println(m);
+					}
 					return 0;
 				}
 			}
+			
 			}
 		return 0;
 	}
-
+	
 	
 
 }
