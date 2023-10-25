0e0e7deeace6a8b862e9ddd589939ca89c27d576
S <s>
2023-10-26 00:09:05 +0400

    {"types":"C","runs":24}

--- Problem28.java
+++ Problem28.java
@@ -9,10 +9,13 @@ public class Problem28 extends ConsoleProgram {
 
 	private int readPositiveInts(String string) {
 		int NOE = 0;		// Number Of Even numbers
+		int a = 0;
 		int n = readInt(string);
 		if(n >= 0){
 			while(n >= 0){
 				int m = readInt(string);
+				a = a+1;
+				println(a);
 				if(m < 0){
 					
 					return 0;
