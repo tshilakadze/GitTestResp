05be814ec75cf9e57737a18725abe66ab176496b
S <s>
2023-10-26 00:26:08 +0400

    {"types":"C","runs":35}

--- Problem28.java
+++ Problem28.java
@@ -3,28 +3,6 @@ import acm.program.ConsoleProgram;
 public class Problem28 extends ConsoleProgram {
 	public void run() {
 		
-		int n = readPositiveInts("Enter a number ");
-		
 	}
-
-	private int readPositiveInts(String string) {
-		int n = readInt(string);
-		if(n >= 0){
-			while(n >= 0){
-				int m = readInt(string);
-				
-				if(m < 0){
-					return 0;
-				}
-				if(m >= 0 && m % 2 == 0){
-					println(m);
-				}
-			}
-			
-			}
-		return 0;
-	}
-	
-	
 
 }
