d07264ccc100ac42ea91d424a8a63fc752b57a08
S <s>
2023-10-25 23:15:44 +0400

    {"types":"C","runs":0}

--- Problem27.java
+++ Problem27.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem27 {
+public class Problem27 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter the first number: ");
+		int m = readInt("Enter the second number: ");
+		int a = a(n, m);
+		println("n^m = " + a);
+		
+	}
 
+	private int a(int n, int m) {
+		for(int i = 0; i <= m; i++){
+		int b = 1;
+		b *= n;	
+		return b;
+		}
+		return 0;
+	}
+	
 }
