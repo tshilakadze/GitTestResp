537223cbd9b8bf241f44eee18ac9290fc568c7bf
S <s>
2023-10-25 23:51:14 +0400

    {"types":"C","runs":15}

--- Problem28.java
+++ Problem28.java
@@ -3,14 +3,10 @@ import acm.program.ConsoleProgram;
 public class Problem28 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter a number ");
+		if(n >= 0){
 		while(n >= 0){
 			readInt("Enter a number ");
-			if(n <= 0){
-				println("Nooooooooooooooooooooooo");
-			}
 		}
-		if(n <= 0){
-			println("Nooooooooooooooooooooooo");
 		}
 		
 	}
