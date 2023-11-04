49c8dbd5693d4a0cd95511d50f80e471d20aa98b
S <s>
2023-11-04 19:28:36 +0400

    {"types":"C","runs":19}

--- Problem33.java
+++ Problem33.java
@@ -2,6 +2,28 @@ import acm.program.ConsoleProgram;
 
 public class Problem33 extends ConsoleProgram {
 	public void run() {
+		int n = readInt("Enter a number: ");
+		if( n != 1){
+			makeItOne(n);
+			
+			}
+		else{
+			println("The number is already 1");
+		}
+		
+		
+	}
+
+	private void makeItOne(int n) {
+		for(int i = n; i == 1; i++){
+			if(i % 2 == 0){
+				println(i/2);
+			}
+			else{
+				println(3*n + 1);
+			}
+		}
+		
 	}
 
 }
