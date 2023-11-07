87c2241eefe89d3e88dda470400aa05c84d90948
S <s>
2023-11-07 17:52:13 +0400

    {"types":"C","runs":13}

--- Problem39.java
+++ Problem39.java
@@ -6,11 +6,22 @@ public class Problem39 extends GraphicsProgram {
 	private static final int SIZE = 100;
 
 	public void run() {
+		drawCircle();
+		
+		
+		
+		
+		
 		GOval ball = new GOval(SIZE, SIZE);
 		add(ball, 0, getHeight()/2 - SIZE/2);
 		while(ball.getX() <= getWidth() - SIZE - 10){
 			ball.move(10, 0);
 			pause(15);
 		}
+	}
+
+	private void drawCircle() {
+		GOval ball = new GOval(SIZE, SIZE);
+		add(ball, 0, getHeight()/2 - SIZE/2);		
 	}
 }
