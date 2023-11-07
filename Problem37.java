ad869e453784ffa4d471ef8e159712f6be79e1e0
S <s>
2023-11-07 16:38:40 +0400

    {"types":"C","runs":4}

--- Problem37.java
+++ Problem37.java
@@ -4,10 +4,18 @@ import acm.util.RandomGenerator;
 public class Problem37 extends ConsoleProgram {
 	private RandomGenerator casino = RandomGenerator.getInstance();
 	public void run(){
-		int myNum = readInt("I choose number ");
+		int myNum = readInt("Choose number: ");
 		while(myNum < 0 || myNum > 36){
 			println("This number is not allowed.");
-			myNum = readInt("I choose number ");
+			myNum = readInt("Choose number: ");
+		}
+		int num = casino.nextInt(0, 36);
+		println("The number is " + num);
+		if(num == myNum){
+			println("You win!");
+		}
+		else{
+			println("You lose! Try again.");
 		}
 	}
 
