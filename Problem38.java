5e26f171a1cd20b53df7cc184a145109c04550b4
S <s>
2023-11-07 16:43:06 +0400

    {"types":"C","runs":7}

--- Problem38.java
+++ Problem38.java
@@ -1,6 +1,8 @@
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 public class Problem38 extends ConsoleProgram {
 	private static final int MYMONEY = 1000;
+	private RandomGenerator casino = RandomGenerator.getInstance();
 	public void run(){
 		
 	}
