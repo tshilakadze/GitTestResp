e3b8b650bf3d4a419839e106373fe38a2def6f03
S <s>
2023-11-07 17:09:00 +0400

    {"types":"C","runs":0}

--- Problem38.java
+++ Problem38.java
@@ -5,6 +5,29 @@ public class Problem38 extends ConsoleProgram {
 	private RandomGenerator casino = RandomGenerator.getInstance();
 	public void run(){
 		
+		int myBet = readInt("Choose your bet: ");
+		int left = MYMONEY - myBet;
+		
+		while(myBet <= 0 || myBet > MYMONEY){
+			println("no");
+		}
+		
+		int myNum = readInt("Choose number: ");
+		
+		while(myNum < 0 || myNum > 36){
+			println("This number is not allowed.");
+			myNum = readInt("Choose number: ");
+		}
+		
+		int num = casino.nextInt(0, 36);
+		println("The number is " + num);
+		if(num == myNum){
+			println("You won " + 2*myBet);
+		}
+		else{
+			println("You lose! Try again! You have " + left + " left.");
+		}
+		
 	}
 
 }
