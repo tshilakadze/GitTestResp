ff3c902caaa83b4d4c4e6a3cf71768ce93624a4d
S <s>
2023-11-07 17:43:22 +0400

    {"types":"C","runs":2}

--- Problem39.java
+++ Problem39.java
@@ -1,4 +1,11 @@
+import acm.program.GraphicsProgram;
 
-public class Problem39 {
+import acm.program.*;
 
+public class Problem39 extends GraphicsProgram {
+	private static final int SIZE = 100;
+
+	public void run() {
+		Glabel ball = new GLabel(SIZE, SIZE);
+	}
 }
