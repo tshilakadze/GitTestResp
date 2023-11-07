68798f2b2ecb667532e5fc48b06942a655151d54
S <s>
2023-11-07 17:56:22 +0400

    {"types":"C","runs":16}

--- Problem39.java
+++ Problem39.java
@@ -5,27 +5,18 @@ import acm.graphics.*;
 public class Problem39 extends GraphicsProgram {
 	private static final int SIZE = 100;
 
-	public void run() {
-		drawCircle();
-		moveCircle();
-		
-		
-		
-		
+	public void run() {		
 		GOval ball = new GOval(SIZE, SIZE);
 		add(ball, 0, getHeight()/2 - SIZE/2);
 		while(ball.getX() <= getWidth() - SIZE - 10){
 			ball.move(10, 0);
 			pause(15);
+			if(ball.getX() == getWidth() - SIZE - 10){
+				ball.move(-10, 0);
+				pause(15);
+			}
 		}
 	}
 
-	private void moveCircle() {
-			
-	}
-
-	private void drawCircle() {
-		GOval ball = new GOval(SIZE, SIZE);
-		add(ball, 0, getHeight()/2 - SIZE/2);		
-	}
+	
 }
