dcd195c572e330557fd5f12e3aa4c8ace6355238
S <s>
2023-11-09 17:15:57 +0400

    {"types":"C","runs":7}

--- GraphicsEventsSample.java
+++ GraphicsEventsSample.java
@@ -1,5 +1,7 @@
 import acm.program.GraphicsProgram;
 
+import java.awt.event.MouseEvent;
+
 import acm.graphics.*;
 
 public class GraphicsEventsSample extends GraphicsProgram {
@@ -11,6 +13,18 @@ public class GraphicsEventsSample extends GraphicsProgram {
 		GRect rect2 = new GRect(100, 100);
 		rect2.setFilled(true);
 		add(rect2, 200, 200);
+		
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e){
+	//	e.getX();
+		
+		GObject obj = getElementAt(e.getX(), e.getY());
+		
+		if(obj != null){
+			pbj.setColor(Color.RED);
+		}
 	}
 
 }
