b2084f5236e48880c989e6e56a86c03c5751b8d4
S <s>
2023-11-09 17:59:23 +0400

    {"types":"C","runs":24}

--- Bonus13_3.java
+++ Bonus13_3.java
@@ -1,5 +1,5 @@
 import acm.program.GraphicsProgram;
-
+import acm.util.RandomGenerator;
 import acm.graphics.*;
 
 public class Bonus13_3 extends GraphicsProgram {
@@ -8,6 +8,8 @@ public class Bonus13_3 extends GraphicsProgram {
 	private static final int DELAY = 20;
 	private static final double VXMAX = 5;
 	private static final double VYMAX = 5;
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public void run() {
 		GOval ball = new GOval(DIAMETER, DIAMETER);
