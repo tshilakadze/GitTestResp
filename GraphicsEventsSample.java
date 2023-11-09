68075f69cf04ce1e6a5e05b6cca4639cdf6b7bf4
S <s>
2023-11-09 17:07:27 +0400

    {"types":"C","runs":7}

--- GraphicsEventsSample.java
+++ GraphicsEventsSample.java
@@ -1,4 +1,16 @@
+import acm.program.GraphicsProgram;
 
-public class GraphicsEventsSample {
+import acm.graphics.*;
+
+public class GraphicsEventsSample extends GraphicsProgram {
+	public void run() {
+		GRect rect1 = new GRect(100, 100);
+		rect1.setFilled(true);
+		add(rect1);
+		
+		GRect rect2 = new GRect(100, 100);
+		rect2.setFilled(true);
+		add(rect2, 200, 200);
+	}
 
 }
