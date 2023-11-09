510dba0c199bab3d0ddb0d1d510a9744ad7e90ff
S <s>
2023-11-09 17:43:52 +0400

    {"types":"C","runs":15}

--- Seminari13damatebiti.java
+++ Seminari13damatebiti.java
@@ -1,6 +1,17 @@
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class Seminari13damatebiti extends GraphicsProgram {
-	
-
+	public void run() {
+		double x1 = 0;
+		double y1 = getHeight() / 2;
+		double x2 = getWidth();
+		double y2 = getHeight() / 2;
+		while(true){
+			removeAll();
+			GLine mLine = new GLine(x1, y1, x2, y2);
+			add(mLine);
+		}
+		
+	}
 }
