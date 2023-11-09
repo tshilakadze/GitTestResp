5efada8665ce55bcbd2755dc3122f89d1e26ece2
S <s>
2023-11-09 17:56:13 +0400

    {"types":"C","runs":24}

--- Bonus13_3.java
+++ Bonus13_3.java
@@ -5,6 +5,9 @@ import acm.graphics.*;
 public class Bonus13_3 extends GraphicsProgram {
 	
 	private static final int DIAMETER = 50;
+	private static final int DELAY = 20;
+	private static final double VXMAX = 5;
+	private static final double VYMAX = 5;
 	
 	public void run() {
 		GOval ball = new GOval(DIAMETER, DIAMETER);
