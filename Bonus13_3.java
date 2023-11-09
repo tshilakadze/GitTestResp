b26de1e2c4f450bb71bee1eeef308e51aee0076a
S <s>
2023-11-09 17:53:52 +0400

    {"types":"C","runs":20}

--- Bonus13_3.java
+++ Bonus13_3.java
@@ -1,4 +1,13 @@
+import acm.program.GraphicsProgram;
 
-public class Bonus13_3 {
+import acm.graphics.*;
 
+public class Bonus13_3 extends GraphicsProgram {
+	
+	private static final int DIAMETER = 50;
+	
+	public void run() {
+		GOval ball = new GOval(DIAMETER, DIAMETER);
+		add(ball, getWidth()/2 - DIAMETER/2, getHeight()/2 - DIAMETER/2);
+	}
 }
