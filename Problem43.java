caf09891912d00ba96f7e666cfe5361990bd3277
S <s>
2023-11-14 17:40:38 +0400

    {"types":"C","runs":43}

--- Problem43.java
+++ Problem43.java
@@ -8,13 +8,14 @@ public class Problem43 extends GraphicsProgram {
 	private GOval circle;
 	private static final int DIAMETER = 50;
 	public void run() {
+		circle = new GOval(DIAMETER, DIAMETER);
+		circle.setFilled(true);
 		add(circle, getWidth()/2, 0);
 		addMouseListeners();
 	}
 	
 	public void mousePressed(MouseEvent e) {
-		circle = new GOval(DIAMETER, DIAMETER);
-		circle.setFilled(true);
+		
 	}
 	
 	public void mouseDragged(MouseEvent e) {
