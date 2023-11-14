c2797c29f87f4120b3385a054b87ab4d0af794aa
S <s>
2023-11-14 17:46:41 +0400

    {"types":"C","runs":48}

--- Problem43.java
+++ Problem43.java
@@ -18,7 +18,7 @@ public class Problem43 extends GraphicsProgram {
 	
 	public void mousePressed(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
-		if(obj == null){
+		if(obj != null){
 			prevX = e.getX();
 			prevY = e.getY();
 		}
