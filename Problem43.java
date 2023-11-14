d277cbd4f081e1884a4352dec03f371ecb77080f
S <s>
2023-11-14 17:36:46 +0400

    {"types":"C","runs":41}

--- Problem43.java
+++ Problem43.java
@@ -5,5 +5,17 @@ import acm.graphics.*;
 import java.awt.event.*;
 
 public class Problem43 extends GraphicsProgram {
-
+	GOval circle;
+	private static final int DIAMETER = 50;
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		
+	}
 }
