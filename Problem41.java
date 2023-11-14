b003a22fdcdbc957d79da3b707d065a02c5c9b40
S <s>
2023-11-14 17:04:43 +0400

    {"types":"C","runs":21}

--- Problem41.java
+++ Problem41.java
@@ -2,6 +2,20 @@ import acm.program.GraphicsProgram;
 
 import acm.graphics.*;
 
+import java.awt.event.*;
+
 public class Problem41 extends GraphicsProgram {
+	
+	private static final int SIZE = 10;
+	
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void MouseDragged(MouseEvent e){
+		GOval ball = new GOval(SIZE, SIZE);
+		ball.setFilled(true);
+		add(ball);
+	}
 	
 }
