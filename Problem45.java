4520d08320dbf45d6cd65e9ab09e13f108bad1a4
S <s>
2023-11-16 17:39:09 +0400

    {"types":"C","runs":18}

--- Problem45.java
+++ Problem45.java
@@ -5,6 +5,14 @@ public class Problem45 extends ConsoleProgram {
 		String word = readLine("the word: ");
 		String symbol = readLine("the symbol: ");
 		
-	
+		char sym = symbol.charAt(0);
+		int num = 0;
+		for(int i = 0; i < word.length(); i++) {
+			if(word.charAt(i) == sym){
+				num++;
+			}
+		}
+		
+		println(num);
 	}
 }
