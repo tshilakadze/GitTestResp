1b980fbdab085196949af0776960f349c0374801
S <s>
2023-11-25 17:56:12 +0400

    {"types":"C","runs":9}

--- Problem45.java
+++ Problem45.java
@@ -3,5 +3,6 @@ import acm.program.ConsoleProgram;
 public class Problem45 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("The text: ");
+		char symbol = readLine("The symbol: ").charAt(0);
 	}
 }
