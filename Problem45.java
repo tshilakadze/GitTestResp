52b52b8237a19879b5fdcf1589c57ffd87bb965b
S <s>
2023-11-25 16:52:48 +0400

    {"types":"C","runs":5}

--- Problem45.java
+++ Problem45.java
@@ -2,17 +2,6 @@ import acm.program.ConsoleProgram;
 
 public class Problem45 extends ConsoleProgram {
 	public void run() {
-		String word = readLine("the word: ");
-		String symbol = readLine("the symbol: ");
 		
-		char sym = symbol.charAt(0);
-		int num = 0;
-		for(int i = 0; i < word.length(); i++) {
-			if(word.charAt(i) == sym){
-				num++;
-			}
-		}
-		
-		println(num);
 	}
 }
