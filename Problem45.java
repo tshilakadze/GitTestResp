0b146ab5868ea0ae795553ca972b8635e1b2fe92
S <s>
2023-11-25 17:39:17 +0400

    {"types":"C","runs":7}

--- Problem45.java
+++ Problem45.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Problem45 extends ConsoleProgram {
 	public void run() {
-		String text = "bla";
+		String text = "Don't fail your midterms";
 		println(text);
 	}
 }
