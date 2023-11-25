d183365f0c912d9c1d9b121e75fbe5ea53610260
S <s>
2023-11-26 01:31:43 +0400

    {"types":"C","runs":35}

--- Problem48.java
+++ Problem48.java
@@ -12,9 +12,12 @@ public class Problem48 extends ConsoleProgram {
 	private void countSymbols(String text, char charAt) {
 		int symNum = 0;
 		for(int i = 0; i < text.length(); i++){
-			if(text.charAt(i) == charAt){
-				symNum = symNum + 1;
+			for(int l = 0; i <= i; l++){
+				if(text.charAt(i) == charAt && text.charAt(i) != text.charAt(i - l)){
+					symNum = symNum + 1;
+				}
 			}
+			
 		}
 		println(symNum);
 	}
