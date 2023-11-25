1572f729433767700682d167d3b31e7429c077ca
S <s>
2023-11-26 00:48:28 +0400

    {"types":"C","runs":22}

--- Problem47.java
+++ Problem47.java
@@ -1,7 +1,15 @@
 import acm.program.ConsoleProgram;
 public class Problem47 extends ConsoleProgram {
 	public void run() {
-		
+		String text = readLine("The text: ");
+		for(int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == text.charAt(text.length() - i)){
+				println("it is a palindrome");
+			}
+			else{
+				println("not a palindrome");
+			}
+		}
 		
 	}
 }
