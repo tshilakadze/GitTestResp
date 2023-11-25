e16a9631db0652814411b921d489e9bbcbd671c5
S <s>
2023-11-26 01:22:50 +0400

    {"types":"C","runs":34}

--- Problem48.java
+++ Problem48.java
@@ -3,10 +3,14 @@ import acm.program.ConsoleProgram;
 public class Problem48 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("The text: ");
-		int 
+		int symNum = 0;
 		for(int i = 0; i < text.length(); i++){
-			
+			countSymbols(text, text.charAt(i));
 		}
+		
+	}
+
+	private void countSymbols(String text, char charAt) {
 		
 	}
 
