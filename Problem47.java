c69f563ed270a3674e4a45bd233802a5820576ff
S <s>
2023-11-26 00:52:42 +0400

    {"types":"C","runs":24}

--- Problem47.java
+++ Problem47.java
@@ -2,14 +2,16 @@ import acm.program.ConsoleProgram;
 public class Problem47 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("The text: ");
-		for(int i = 0; i < text.length(); i++){
+		palindromeOrNot(text);
+		
+	}
+
+	private boolean palindromeOrNot(String text) {
+		for(int i = 0; i < text.length()/2; i++){
 			if(text.charAt(i) == text.charAt(text.length() - i)){
-				println("it is a palindrome");
-			}
-			else{
-				println("not a palindrome");
+				return true;
 			}
 		}
-		
+		return false;
 	}
 }
