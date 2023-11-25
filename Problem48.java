c6d7272e5cc5333e2762c33d9f371c5029b2bf35
S <s>
2023-11-25 16:53:50 +0400

    {"types":"C","runs":5}

--- Problem48.java
+++ Problem48.java
@@ -2,12 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Problem48 extends ConsoleProgram {
 	public void run() {
-		String text = "tsotne";
-		printCharStatistics(text);
-	}
-
-	private void printCharStatistics(String text) {
+		
 		
 	}
+
+	
 	
 }
