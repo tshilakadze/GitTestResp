40b62f075530d164db32a1d8b82e30738a66e8f9
S <s>
2023-11-26 00:57:10 +0400

    {"types":"C","runs":26}

--- Problem47.java
+++ Problem47.java
@@ -7,12 +7,7 @@ public class Problem47 extends ConsoleProgram {
 	}
 
 	private boolean palindromeOrNot(String text) {
-		for(int i = 0; i < text.length()/2; i++){
-			if(text.charAt(i) == text.charAt(text.length() - i)){
-				
-			}
-			return true;
-		}
+		
 		return false;
 	}
 }
