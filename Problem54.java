22b4af2f9f6468790bbe767c3b612d07e91d4d29
S <s>
2023-11-26 21:13:50 +0400

    {"types":"C","runs":38}

--- Problem54.java
+++ Problem54.java
@@ -1,4 +1,14 @@
+import acm.program.*;
+public class Problem54 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("The number n = ");
+		typeInNumbers(n);
+	}
 
-public class Problem54 {
+	private void typeInNumbers(int n) {
+		for(int i = 0; i < n; i++){
+			int a = readInt("? ");
+		}
+	}
 
 }
