151907b1abe941989f07267d3773e57b66b7bc57
S <s>
2023-11-26 16:20:36 +0400

    {"types":"C","runs":13}

--- Problem49.java
+++ Problem49.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem49 {
+public class Problem49 extends ConsoleProgram {
+	
+	public void run() {
+		String numStr = readLine("Enter a number: ");
+		int finalNum = stringToInteger(numStr);
+	}
 
+	private int stringToInteger(String numStr) {
+		for(int i = 0; i < numStr.length(); i++){
+			int currDigit = charToInteger(numStr.charAt(i));
+		}
+		return 0;
+	}
+
+	private int charToInteger(char charAt) {
+		
+		return charAt - '0';
+	}
 }
