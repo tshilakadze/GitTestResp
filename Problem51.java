9603804076621cc068b88375704e9cf82c7417da
S <s>
2023-11-26 21:04:31 +0400

    {"types":"C","runs":32}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,20 @@
+import acm.program.*;
+public class Problem51 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter a text: ");
+		printNewText(text);
+	}
 
-public class Problem51 {
+	private void printNewText(String text) {
+		String used = "";
+		for(int i = 0; i < text.length(); i++){
+			char ch = text.charAt(i);
+			if(!used.contains("" + ch)){
+				used += ch;
+			}
+		}
+		println(used);
+		
+	}
 
 }
