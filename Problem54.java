bd2c339478dbd1882704fda8b8741353947e1561
S <s>
2023-11-26 21:22:14 +0400

    {"types":"C","runs":40}

--- Problem54.java
+++ Problem54.java
@@ -6,9 +6,22 @@ public class Problem54 extends ConsoleProgram {
 	}
 
 	private void typeInNumbers(int n) {
+		int max = 0;
+		int secMax = 0;
 		for(int i = 0; i < n; i++){
 			int a = readInt("? ");
+			if(a > Integer.MIN_VALUE && a < Integer.MAX_VALUE){
+				max = a;
+				secMax = a;
+				checkNext(a, max, secMax);
+			}
 		}
+		println("The largest number is " + max);
+		println("The second largest number is " + secMax);
+	}
+
+	private void checkNext(int a, int max, int secMax) {
+		
 	}
 
 }
