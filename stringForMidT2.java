7a37e8f4294f23739869a99559f4a6b05a439bc9
S <s>
2023-11-27 21:09:54 +0400

    {"types":"C","runs":33}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -2,8 +2,8 @@ import acm.program.*;
 public class stringForMidT2 extends ConsoleProgram {
 	public void run() {
 		String IP = readLine("adress = ");
-		char a = '[';
 		IP.replace('.', '[');
+		
 	}
 
 	
