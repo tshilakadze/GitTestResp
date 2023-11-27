5d545fa28360f45265b3dbb7e038bcc5da368b27
S <s>
2023-11-27 20:27:21 +0400

    {"types":"C","runs":17}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -12,7 +12,7 @@ public class stringForMidT2 extends ConsoleProgram {
 				String firstPart = IP.substring(0, i);
 				String secondPart = IP.substring(i + 1);
 				result = firstPart + "[.]" + secondPart;
-				IP = result;
+				result = IP;
 			}
 		}
 		println(IP);
