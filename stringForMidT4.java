57963968840cc8a1242739ab3eb8beb5714ad8ae
S <s>
2023-11-27 23:59:35 +0400

    {"types":"C","runs":49}

--- stringForMidT4.java
+++ stringForMidT4.java
@@ -5,13 +5,18 @@ public class stringForMidT4 extends ConsoleProgram {
 		String text = readLine("Input: ");
 		for(int i = 0; i < text.length(); i++){
 			if(text.charAt(i) == 'i'){
-				reverseString(text, i);
+				String part1 = text.substring(0, i);
+				reverseString(part1, text);
 			}
 		}
 	}
 
-	private void reverseString(String text, int i) {
-		
+	private String reverseString(String part1, String text) {
+		String result = "";
+		for(int i = 0; i < part1.length(); i++){
+			
+		}
+		return result;
 	}
 
 }
