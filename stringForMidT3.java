d036f0353e80267a7c5b5b5279662ce73bfa6a9c
S <s>
2023-11-27 22:49:32 +0400

    {"types":"C","runs":44}

--- stringForMidT3.java
+++ stringForMidT3.java
@@ -1,4 +1,16 @@
-
-public class stringForMidT3 {
+import acm.program.*;
+public class stringForMidT3 extends ConsoleProgram {
+	public void run() {
+		String jewels = readLine("jewels: ");
+		String myStones = readLine("my stones: ");
+		int jewelNum = 0;
+		for(int i = 0; i < myStones.length(); i++){
+			for(int l = 0; l < jewels.length(); l++){
+				if(myStones.charAt(i) == jewels.charAt(l)){
+					jewelNum += 1;
+				}
+			}
+		}
+	}
 
 }
