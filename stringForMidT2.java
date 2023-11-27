1b2e8a2004337a55afd6ab2ea37944e2a4ac9a4a
S <s>
2023-11-27 20:10:50 +0400

    {"types":"C","runs":9}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -3,7 +3,7 @@ public class stringForMidT2 extends ConsoleProgram {
 	public void run() {
 		String IP = readLine("adress = ");
 		replacePoints(IP);
-		
+		println(IP);
 	}
 
 	private String replacePoints(String IP) {
