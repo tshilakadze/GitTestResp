749be07994eb7792185bb68d5908d6e76d952dc9
S <s>
2023-11-27 21:45:12 +0400

    {"types":"C","runs":42}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -2,39 +2,8 @@ import acm.program.*;
 public class stringForMidT2 extends ConsoleProgram {
 	public void run() {
 		String IP = readLine("adress = ");
-		findPoints(IP);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//	IP = IP.replaceAll(".", "[.]");
-		
-	//	println(IP);
-	}
-
-	private void findPoints(String IP) {
-		for(int i = 0; i < IP.length(); i++){
-			if(IP.charAt(i) == '.'){
-				String firstPart = IP.substring(0, i);
-				String secondPart = IP.substring(i + 1);
-				IP = firstPart + "[.]" + secondPart;
-				println(IP);
-				
-			}
-		}
+		IP = IP.replaceAll(".", "[.]");
+		println(IP);
 	}
 
 	
