6ba5aea667029feb0e5c16168b14ab3b255a18aa
S <s>
2023-11-27 21:42:52 +0400

    {"types":"C","runs":41}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -2,9 +2,39 @@ import acm.program.*;
 public class stringForMidT2 extends ConsoleProgram {
 	public void run() {
 		String IP = readLine("adress = ");
-		IP = IP.replaceAll(".", "[.]");
+		findPoints(IP);
 		
-		println(IP);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//	IP = IP.replaceAll(".", "[.]");
+		
+	//	println(IP);
+	}
+
+	private void findPoints(String IP) {
+		for(int i = 0; i < IP.length(); i++){
+			if(IP.charAt(i) == '.'){
+				String firstPart = IP.substring(0, i);
+				String secondPart = IP.substring(i + 1);
+				IP = firstPart + "[.]" + secondPart;
+				println(IP);
+				
+			}
+		}
 	}
 
 	
@@ -12,30 +42,3 @@ public class stringForMidT2 extends ConsoleProgram {
 	
 
 }
-
-
-
-
-
-
-/*
- * private void findPoints(String IP) {
-		String result = "";
-		for(int i = 0; i < IP.length(); i++){
-			if(IP.charAt(i) == '.'){
-				String firstPart = IP.substring(0, i);
-				String secondPart = IP.substring(i + 1);
-				result = firstPart + "[.]" + secondPart;
-				for(int l = 0; l < secondPart.length(); l++){
-					if(secondPart.charAt(l) == '.'){
-						firstPart = secondPart.substring(0, l);
-						secondPart = secondPart.substring(l + 1);
-						result = firstPart + "[.]" + secondPart;
-					}
-					println(result);
-				}
-				
-			}
-		}
-	}
- */
