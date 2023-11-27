7aae5279c01d119aaa6f33575ab7131635368394
S <s>
2023-11-27 20:30:18 +0400

    {"types":"C","runs":24}

--- stringForMidT2.java
+++ stringForMidT2.java
@@ -13,10 +13,10 @@ public class stringForMidT2 extends ConsoleProgram {
 				String secondPart = IP.substring(i + 1);
 				result = firstPart + "[.]" + secondPart;
 				println(result);
-				findPoints(IP);
+				
 			}
 		}
-		
+		findPoints(IP);
 		
 	}
 
