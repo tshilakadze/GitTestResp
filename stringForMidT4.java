5954cc826d07f2709b7e5d28fb54353cd7f2f396
S <s>
2023-11-28 16:30:46 +0400

    {"types":"C","runs":15}

--- stringForMidT4.java
+++ stringForMidT4.java
@@ -7,9 +7,9 @@ public class stringForMidT4 extends ConsoleProgram {
 		for(int i = 0; i < text.length(); i++){
 			if(text.charAt(i) == 'i'){
 				String part1 = text.substring(0, i);
-				String part2 = text.substring(i + 1);
+				String part2 = text.substring(i);
 				for(int l = 0; l < part1.length(); l++){
-					part1 = part1.replace(part1.charAt(l), part1.charAt(part1.length() - l - 1));
+					part1 = part1.replace(part1.charAt(l), text.charAt(text.length() - part2.length() - l - 1));
 					result = part1;
 				}
 			}
