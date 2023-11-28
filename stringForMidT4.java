486fef56e0b806d1f96450a662b1cf29ee324a75
S <s>
2023-11-28 16:16:35 +0400

    {"types":"C","runs":5}

--- stringForMidT4.java
+++ stringForMidT4.java
@@ -1,22 +1,23 @@
 import acm.program.*;
 
 public class stringForMidT4 extends ConsoleProgram {
+	String result;
 	public void run() {
 		String text = readLine("Input: ");
 		for(int i = 0; i < text.length(); i++){
 			if(text.charAt(i) == 'i'){
 				String part1 = text.substring(0, i);
-				reverseString(part1, text);
+				reverseString(part1);
 			}
 		}
+		println(result);
 	}
 
-	private void reverseString(String part1, String text) {
-		String result = "";
+	private void reverseString(String part1) {
+		result = "";
 		for(int i = 0; i < part1.length(); i++){
 			result = part1.replace(part1.charAt(i), part1.charAt(part1.length() - i));
 		}
-		println(result);
 	}
 
 }
