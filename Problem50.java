556b0132a797d87ab976a6f98ffe24813e85e901
S <s>
2023-11-28 17:26:43 +0400

    {"types":"C","runs":36}

--- Problem50.java
+++ Problem50.java
@@ -8,7 +8,6 @@ public class Problem50 extends ConsoleProgram {
 		String text = readLine("Enter the text: ");
 		StringTokenizer tokenizer = new StringTokenizer(text);
 		while(tokenizer.hasMoreTokens()){
-		//	String word = tokenizer.nextToken();
 			tokenizer.nextToken();
 			numOfWords += 1;
 		}
