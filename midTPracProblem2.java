dd0f28c6c3c42f6d118bf91e07d0ae06cdca4694
S <s>
2023-11-28 18:01:36 +0400

    {"types":"C","runs":37}

--- midTPracProblem2.java
+++ midTPracProblem2.java
@@ -1,4 +1,13 @@
-
-public class midTPracProblem2 {
+import acm.program.*;
+public class midTPracProblem2 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("enter text: ");
+		for(int i = 0; i < text.length(); i++){
+			char ch = text.charAt(i);
+			if(Character.isLowerCase(ch)){
+			
+			}
+		}	
+	}
 
 }
