6b97f5ee538b03c8ac98061eade4a53ed2b08191
S <s>
2023-11-30 17:21:00 +0400

    {"types":"C","runs":1}

--- midTPracProblem1.java
+++ midTPracProblem1.java
@@ -1,5 +1,13 @@
 import acm.program.*;
 
-public class midTPracProblem1 extends ConsoleProgram {
 
+/*
+ * This is a solution for problem 2.
+ */
+
+public class midTPracProblem1 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("enter the text = ");
+		
+	}
 }
