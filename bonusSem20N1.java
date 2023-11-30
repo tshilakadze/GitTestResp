127812cf3ff1e5d171a8a2c733e02246d9cdd757
S <s>
2023-11-30 17:28:13 +0400

    {"types":"C","runs":1}

--- bonusSem20N1.java
+++ bonusSem20N1.java
@@ -9,10 +9,16 @@ public class bonusSem20N1 extends GraphicsProgram {
 	private static final double MAXRADIUS = 50;
 	private static final double MINSPEED = 1;
 	private static final double MAXSPEED = 5;
-	
+	private static final int DELAY = 50;
+	private GOval circle = null;
+	private double vx = 0;
+	private double vy = 0;
 	public void run() {
 		addMouseListeners();
 		addRandomCircle();
+		while(true){
+			circle.move(vx, vy);
+		}
 	}
 
 	
@@ -20,7 +26,7 @@ public class bonusSem20N1 extends GraphicsProgram {
 		double radius = rgen1.nextDouble(MINRADIUS, MAXRADIUS);
 		double x = rgen1.nextDouble(0, getWidth() - 2 * radius);
 		double y = rgen1.nextDouble(0, getHeight() - 2 * radius);
-		GOval circle = new GOval(2 * radius, 2 * radius);
+		circle = new GOval(2 * radius, 2 * radius);
 		circle.setFilled(true);
 		circle.setColor(rgen1.nextColor());
 		add(circle, x, y);
