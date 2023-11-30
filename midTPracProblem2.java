bf21ac556cb8b481c623f63624c49aee47b33186
S <s>
2023-12-01 00:58:35 +0400

    {"types":"C","runs":15}

--- midTPracProblem2.java
+++ midTPracProblem2.java
@@ -6,7 +6,7 @@ public class midTPracProblem2 extends ConsoleProgram {
 		String text = readLine("enter text: ");
 		for(int i = 0; i < text.length(); i++){
 			char ch = text.charAt(i);
-			String a = "" + ch
+			String a = "" + ch;
 	//		char nextCh = text.charAt(i + 1);
 			if(Character.isUpperCase(ch)){
 				text.replaceFirst(a, "");
