e11d0f9a99f6726740ea5824e523c1d61d1fc561
S <s>
2023-11-30 23:42:16 +0400

    {"types":"C","runs":0}

--- midTPracProblem2.java
+++ midTPracProblem2.java
@@ -1,13 +1,26 @@
 import acm.program.*;
+
 public class midTPracProblem2 extends ConsoleProgram {
+
 	public void run() {
 		String text = readLine("enter text: ");
 		for(int i = 0; i < text.length(); i++){
 			char ch = text.charAt(i);
 			if(Character.isLowerCase(ch)){
-			
+				if(ascendingOrder(text, ch)){
+					
+				}
 			}
 		}	
+	}
+
+	private boolean ascendingOrder(String text, char ch) {
+		for(int i = 0; i < text.length(); i++){
+			if(text.charAt(i) < text.charAt(i + 1)){
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
