ba1cd5ddd9f9deac4b003846b114ba41edf9c23d
S <s>
2023-12-01 23:06:19 +0400

    {"types":"C","runs":86}

--- testAgain.java
+++ testAgain.java
@@ -3,28 +3,59 @@ import acm.program.ConsoleProgram;
 import java.util.*;
 
 public class testAgain extends ConsoleProgram {
+	private static final String ZEROS = "00000000000000000000000000000000";
+
 	public void run() {
 		while (true) {
-			String s = readLine("Enter text: ");
-			println(beautify(s));
+			int num = readInt("Enter number: ");
+			println(reverseBinNum(num));
 		}
 	}
 
-	private String beautify(String s) {
-		StringTokenizer tokenizer = new StringTokenizer(s, " ,.", true);
-		String beautifulS = "";
-		while (tokenizer.hasMoreTokens()) {
-			String currentWord = tokenizer.nextToken();
-			beautifulS += reversed(currentWord);
-		}
-		return beautifulS;
+	// we should save result in long to be able to save ints up to 2^32
+	private long reverseBinNum(int num) {
+		String binaryStr = IntToBinaryStr(num);
+		String reveredBinaryStr = reversedBinaryString(binaryStr);
+		println(binaryStr);
+		println(reveredBinaryStr);
+		long result = binaryStrToInt(reveredBinaryStr);
+		return result;
 	}
 
-	private String reversed(String word) {
-		String reversed = "";
-		for(int i = word.length() -1; i >= 0; i--){
-			reversed += word.charAt(i);
+	private String IntToBinaryStr(int num) {
+		String result = ZEROS; // 32 zeros
+		int i = result.length() - 1; // 32 - 1 -- max index
+		// we start setting binary digits from the end of the string
+		while (num > 0) {
+			int currBinaryDigit = (num % 2);
+			if (currBinaryDigit == 1) {
+				result = result.substring(0, i) + '1' + result.substring(i + 1);
+			}
+			num /= 2;
+			i--;
 		}
-		return reversed;
+		return result;
+	}
+
+	private String reversedBinaryString(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			char reveredCh = '0';
+			if (s.charAt(i) == '0') {
+				reveredCh = '1';
+			}
+			s = s.substring(0, i) + reveredCh + s.substring(i + 1);
+		}
+		return s;
+	}
+
+	private long binaryStrToInt(String str) {
+		long result = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int currDigit = str.charAt(i) - '0';
+			result += currDigit * Math.pow(2, str.length() - 1 - i);
+			// println(currDigit + " * 2^" + (str.length() - 1 - i));
+		}
+
+		return result;
 	}
 }
