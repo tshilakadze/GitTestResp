ed1576ddb53fb9e0cf2ccaa8da82db7e2708568a
S <s>
2023-12-02 01:47:30 +0400

    {"types":"C","runs":125}

--- midTPracProblem12.java
+++ midTPracProblem12.java
@@ -3,6 +3,9 @@ public class midTPracProblem12 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter the number n: ");
 		int k = readInt("Enter the number between 2 and 9: ");
+		while(k < 2 || k > 9){
+			k = readInt("Can you read you fucking imbecile? Enter a proper number you asshole: ");
+		}
 		println(sumOfNumbers(n, k));
 		
 	}
