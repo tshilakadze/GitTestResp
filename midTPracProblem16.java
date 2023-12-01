43cde33e70c2cba92315658fe4cee9e7477b7dda
S <s>
2023-12-02 02:08:08 +0400

    {"types":"C","runs":131}

--- midTPracProblem16.java
+++ midTPracProblem16.java
@@ -1,4 +1,18 @@
+import acm.program.*;
+public class midTPracProblem16 extends ConsoleProgram{
+	public void run() {
+		double n = readInt("n = ");
+		double a = readInt("a ");
+		double q = readInt("q = ");
+		println(findNthMember(n, a, q));
+	}
 
-public class midTPracProblem16 {
-
+	
+	
+	private int findNthMember(double n, double a, double q){
+		n = n - 1;
+		double b = Math.pow(q, n);
+		double nthMember = a * b;
+		return nthMember;
+	}
 }
