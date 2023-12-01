701abc46faa77dab87e901ac9ab4515a436504bf
S <s>
2023-12-01 16:05:22 +0400

    {"types":"C","runs":22}

--- midTPracProblem1.java
+++ midTPracProblem1.java
@@ -8,6 +8,6 @@ import acm.program.*;
 public class midTPracProblem1 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("enter the text = ");
-		
+		text = text.toLowerCase();
 	}
 }
