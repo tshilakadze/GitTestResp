16e4f351f697383c8895da8203536cffcdc2dd57
S <s>
2023-12-01 22:10:35 +0400

    {"types":"C","runs":79}

--- midTPracProblem10.java
+++ midTPracProblem10.java
@@ -1,4 +1,21 @@
-
-public class midTPracProblem10 {
+import acm.program.*;
+public class midTPracProblem10 extends ConsoleProgram {
+	private String a;
+	public void run() {
+		int num = readInt("Enter a number: ");
+		a = "" + num;
+		println(equalsOrNot(num));
+	}
+	
+	private boolean equalsOrNot(int num){
+		int oddSum = 0;
+		int evenSum = 0;
+		for(int i = 0; i < a.length(); i++){
+			char ch = a.charAt(i);
+			int b = ch + 0;
+			println(b);
+		}
+		return true;
+	}
 
 }
