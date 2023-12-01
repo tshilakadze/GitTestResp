091481600f9394531589d0eb49fdbc1bb1488616
S <s>
2023-12-01 17:38:23 +0400

    {"types":"C","runs":36}

--- midTPracProb3.java
+++ midTPracProb3.java
@@ -5,14 +5,29 @@ import java.util.*;
 public class midTPracProb3 extends ConsoleProgram {
 	
 	private String text;
+	private String word;
 	
 	public void run() {
 		text = readLine("Enter text: ");
 		text = text.toLowerCase();
+		seperateWords();
+		
+		
+	}
+	
+	private String seperateWords() {
+		
 		StringTokenizer tokenizer = new StringTokenizer(text);
 		while(tokenizer.hasMoreTokens()){
-			println(tokenizer.nextToken());
+			word = tokenizer.nextToken();
+			String firstLetter = word.substring(0, 1);
+			String restOfWord = word.substring(1);
+			firstLetter.toUpperCase();
+			word = firstLetter + restOfWord;
+			
 		}
+		
+		return word;
 	}
 
 }
