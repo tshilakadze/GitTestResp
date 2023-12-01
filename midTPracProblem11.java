dfdbede8dd5c1764681f1812f9dd5a0ccd1af14b
S <s>
2023-12-01 23:06:28 +0400

    {"types":"C","runs":87}

--- midTPracProblem11.java
+++ midTPracProblem11.java
@@ -1,4 +1,11 @@
-
-public class midTPracProblem11 {
+import acm.program.*;
+public class midTPracProblem11 extends ConsoleProgram {
+	
+	private static final String ZEROS = "00000000000000000000000000000000";
+	
+	public void run() {
+		int num = readInt("Enter a number: ");
+		
+	}
 
 }
