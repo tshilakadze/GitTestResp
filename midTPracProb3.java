bf15a36df01aa41a98dd5960d364578e6c1a50bd
S <s>
2023-12-01 17:23:44 +0400

    {"types":"C","runs":32}

--- midTPracProb3.java
+++ midTPracProb3.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class midTPracProb3 {
+import java.util.*;
+
+public class midTPracProb3 extends ConsoleProgram {
+	
+	private String text;
+	
+	public void run() {
+		text = readLine("Enter text: ");
+		text = text.toLowerCase();
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		println(tokenizer);
+	}
 
 }
