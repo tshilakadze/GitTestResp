3fa907f39965d1fb16a65dbd4f7c33651152d028
S <s>
2023-12-01 18:34:15 +0400

    {"types":"C","runs":62}

--- midTPracProblem4.java
+++ midTPracProblem4.java
@@ -36,7 +36,6 @@ public class midTPracProblem4 extends ConsoleProgram {
 			char ch = word.charAt(word.length() - i - 1);
 			String letter = "" + ch;
 			newWord += letter;
-			newWord = newWord + " ";
 		}
 		return newWord;
 	}
