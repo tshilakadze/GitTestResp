9aeaca43b8f58d0775abc9a3ac338b0a1d2959c7
S <s>
2023-12-01 15:28:00 +0400

    {"types":"C","runs":7}

--- midTPracProblem2.java
+++ midTPracProblem2.java
@@ -1,18 +1,20 @@
 import acm.program.*;
 
 public class midTPracProblem2 extends ConsoleProgram {
+	
+	private String result;
 
 	public void run() {
 		String text = readLine("enter text: ");
 		onlyLowerCase(text);
-		
+		println(result);
 		
 		
 		
 	}
 
 	private String onlyLowerCase(String text) {
-		String result = "";
+		result = "";
 		for(int i = 0; i < text.length(); i++){
 			char ch = text.charAt(i);
 			if(Character.isLowerCase(ch)){
