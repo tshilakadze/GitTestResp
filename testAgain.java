83f26938699437b25a587aec0d061bae25791b8d
S <s>
2023-12-01 15:51:10 +0400

    {"types":"C","runs":15}

--- testAgain.java
+++ testAgain.java
@@ -2,10 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
 	public void run() {
-		String text1 = readLine("text 1: ");
-		String text2 = readLine("Text 2: ");
-		if(text1.equalsIgnoreCase(text2)){
-			println(true);
-		}
+		
+
 	}
 }
