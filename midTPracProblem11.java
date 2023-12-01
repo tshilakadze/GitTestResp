915743dafbec302325369d480b6f3271d421b858
S <s>
2023-12-01 23:23:52 +0400

    {"types":"C","runs":87}

--- midTPracProblem11.java
+++ midTPracProblem11.java
@@ -5,7 +5,23 @@ public class midTPracProblem11 extends ConsoleProgram {
 	
 	public void run() {
 		int num = readInt("Enter a number: ");
+		println(binary(num));
 		
+	}
+	
+	private int binary(int num){
+		String finalNum = "";
+		while(true){
+			int remainder = num % 2;
+			num = num / 2;
+			if(num / 2 == 0){
+				break;
+			}
+			String number = remainder + "";
+			finalNum += number;
+		}
+		int realNumber = Integer.parseInt(finalNum);
+		return realNumber;
 	}
 
 }
