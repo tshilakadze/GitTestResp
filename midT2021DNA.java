d8c692799c70532c3902755e157e2bde6b9dc2af
S <s>
2023-12-02 14:51:14 +0400

    {"types":"A","runs":0}

---
+++ midT2021DNA.java
@@ -0,0 +1,32 @@
+import acm.program.*;
+public class midT2021DNA extends ConsoleProgram {
+	public void run() {
+	String lukeDNA = readLine("Luke's DNA: ");
+	String anakinDNA = readLine("Anakin's DNA: ");
+	lukeDNA = lukeDNA.toUpperCase();
+	anakinDNA = anakinDNA.toUpperCase();
+	println(amIYourFatherLuke(lukeDNA, anakinDNA));
+	
+}
+
+	private boolean amIYourFatherLuke(String lukeDNA, String anakinDNA){
+	String result = "";
+	for(int i = 0; i < lukeDNA.length(); i++){
+		char lCh = lukeDNA.charAt(i);
+		for(int l = 0; l < anakinDNA.length(); l++){
+			char aCh = anakinDNA.charAt(l);
+			if(lCh == aCh){
+				String a = lCh + "";
+				result += a;
+			}
+			else{
+				result = "";
+			}
+		}
+	}
+		if(result.length() >= anakinDNA.length()/2){
+			return true;
+		}
+	return false;
+	}
+}
