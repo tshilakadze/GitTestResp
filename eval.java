a4989600cb4b40481b959d5ed212f170af73d675
S <s>
2023-12-02 18:22:35 +0400

    {"types":"C","runs":2}

--- eval.java
+++ eval.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class eval extends ConsoleProgram{
 	public void run(){
-		String s = "";
+		String s = "2 + 5 + 5 - 3";
 		System.out.println(eval(s));
 	}
 	private int eval(String s){
