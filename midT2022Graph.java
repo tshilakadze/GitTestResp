1e953278be73e94395a6f4a9dfd3bd990a435522
S <s>
2023-12-02 16:35:04 +0400

    {"types":"C","runs":27}

--- midT2022Graph.java
+++ midT2022Graph.java
@@ -1,4 +1,24 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class midT2022Graph {
+public class midT2022Graph extends GraphicsProgram {
+	private static final double ROWS = 8;
+	private static final double COLUMNS = 8;
+	public void run(){
+		drawRects();
+		addMouseListeners();
+		
+	}
+	private void drawRects() {
+		double size = Math.min(getHeight(), getWidth())/COLUMNS;
+		for(int i = 0; i < ROWS; i++){
+			for(int l = 0; l < COLUMNS; l++){
+				int x = i;
+				int y = l;
+				GRect rect = new GRect(size, size);
+				add(rect, x, y);
+			}
+		}
+	}
 
 }
