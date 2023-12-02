de2ad0ece66b75b530cd1cad0b407899eb32782b
S <s>
2023-12-02 12:35:30 +0400

    {"types":"C","runs":0}

--- midT2021log.java
+++ midT2021log.java
@@ -1,4 +1,19 @@
+import acm.program.*;
+public class midT2021log extends ConsoleProgram {
+	public void run(){
+		int n = readInt("Enter the first number: ");
+		int m = readInt("Enter the second number: ");
+		println(findLog(n, m,));
+		
+}
 
-public class midT2021log {
 
+	private int findLog(int n, int m){
+		int result = 0;
+		while(n != 0){
+			n = n / m;
+			result += 1;
+		}
+		return result;
+}
 }
