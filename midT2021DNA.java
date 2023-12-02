ccd55e5d64e4a1ac39ee0d231b85aeb97d681d81
S <s>
2023-12-02 15:01:06 +0400

    {"types":"C","runs":12}

--- midT2021DNA.java
+++ midT2021DNA.java
@@ -14,24 +14,27 @@ public class midT2021DNA extends ConsoleProgram {
 	if(lukeDNA.length() < anakinDNA.length()/2){
 		return false;
 	}
-	for(int i = 0; i < lukeDNA.length(); i++){
-		char lCh = lukeDNA.charAt(i);
-		for(int l = 0; l < anakinDNA.length(); l++){
-			char aCh = anakinDNA.charAt(l);
-			if(lCh == aCh){
-				result += lCh;
-				i = i + 1;
-				l = l + 1;
-			}
-			else{
-				result = "";
+	else{
+		for(int i = 0; i < lukeDNA.length(); i++){
+			char lCh = lukeDNA.charAt(i);
+			for(int l = 0; l < anakinDNA.length(); l++){
+				char aCh = anakinDNA.charAt(l);
+				if(lCh == aCh){
+					result += lCh;
+					i = i + 1;
+					l = l + 1;
+				}
+				else{
+					result = "";
+				}
 			}
 		}
+		println(result);
+			if(result.length() >= anakinDNA.length()/2){
+				return true;
+			}
 	}
-	println(result);
-		if(result.length() >= anakinDNA.length()/2){
-			return true;
-		}
+	
 	return false;
 	}
 }
