fa2cde448ed99c5a4adc07b764a19ea5d4304634
S <s>
2023-12-02 18:22:19 +0400

    {"types":"C","runs":1}

--- eval.java
+++ eval.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class eval {
-
+public class eval extends ConsoleProgram{
+	public void run(){
+		String s = "";
+		System.out.println(eval(s));
+	}
+	private int eval(String s){
+		s.replaceAll(" ", "");
+		int result = 0;
+		for(int i = 0; i < s.length(); i++){
+			if(i == 0){
+				result = s.charAt(0) - '0';
+			}
+			if(s.charAt(i) == '+'){
+				result = result + (s.charAt(i) - '0');
+			}
+			if(s.charAt(i) == '-'){
+				result = result - (s.charAt(i) - '0');
+			}
+			if(s.length() == 1){
+				return s.charAt(0) - '0';
+			}
+		}
+		return result;
+	}
 }
