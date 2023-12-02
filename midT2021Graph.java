d37f78dfccf1efe8501b1bdcf3718d48c0824055
S <s>
2023-12-02 15:55:39 +0400

    {"types":"C","runs":21}

--- midT2021Graph.java
+++ midT2021Graph.java
@@ -1,6 +1,23 @@
+import java.awt.event.MouseEvent;
+
 import acm.program.GraphicsProgram;
 
+import acm.graphics.*;
+
 public class midT2021Graph extends GraphicsProgram {
-	 
+	 private int x;
+	 private int y;
+	public void run() {
+		 x = 0;
+		 y = 0;
+		 addMouseListeners();
+	 }
+	
+	public void mousClicked(MouseEvent e){
+		int x1 = e.getX();
+		int y1 = e.getY();
+		GLine line = new GLine(x, y, x1, y1);
+		add(line);
+	}
 
 }
