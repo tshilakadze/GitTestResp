49648ffc741372528bedbcce8ae686172c021fbe
S <s>
2023-12-02 15:17:13 +0400

    {"types":"C","runs":17}

--- midT2021DNA.java
+++ midT2021DNA.java
@@ -10,31 +10,14 @@ public class midT2021DNA extends ConsoleProgram {
 }
 
 	private boolean amIYourFatherLuke(String lukeDNA, String anakinDNA){
-	String result = "";
-	if(lukeDNA.length() <= anakinDNA.length()/2){
-		return false;
-	}
-	else{
 		for(int i = 0; i < lukeDNA.length(); i++){
-			char lCh = lukeDNA.charAt(i);
-			for(int l = 0; l < anakinDNA.length(); l++){
-				char aCh = anakinDNA.charAt(l);
-				if(lCh == aCh){
-					result += lCh;
-					i = i + 1;
-					l = l + 1;
-				}
-				else{
-					result = "";
+			for(int j = 0; j < anakinDNA.length(); j++){
+				String DNA = lukeDNA.substring(i, j);
+				if(anakinDNA.contains(DNA) && DNA.length() > anakinDNA.length()/2){
+					return true;
 				}
 			}
 		}
-		println(result);
-			if(result.length() > anakinDNA.length()/2){
-				return true;
-			}
-	}
-	
 	return false;
 	}
 }
