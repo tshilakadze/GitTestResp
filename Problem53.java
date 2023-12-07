58c4fd84ddd7561343cf76b46d3d2c99a10dccf2
S <s>
2023-12-07 17:35:49 +0400

    {"types":"C","runs":1}

--- Problem53.java
+++ Problem53.java
@@ -1,4 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem53 {
+import acm.program.*;
 
+public class Problem53 extends ConsoleProgram {
+	
+	private static final String FILENAME = "Statistics.txt";
+	
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
+			String text = "";
+			while(true){
+				String line = reader.readLine();
+				if(line == null){
+					break;
+				}
+				text += line + " ";
+			}
+			countStatistics(text);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void countStatistics(String text) {
+		
+		
+	}
+	
 }
