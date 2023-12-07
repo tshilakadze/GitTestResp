d179e9413ddf69b7cae4b80f1d939882ce2ef767
S <s>
2023-12-07 17:53:50 +0400

    {"types":"C","runs":6}

--- Problem56.java
+++ Problem56.java
@@ -7,7 +7,7 @@ public class Problem56 extends ConsoleProgram {
 	public void run() {
 		n = readInt("? ");
 		int[] arr = new int[n];
-		min = 0;
+	//	min = 0;
 		for(int i = 0; i < n; i++){
 			arr[i] = readInt("Enter a number: ");
 		}
