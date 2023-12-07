d086ceecdfd0f5a5893630ab89a29da6a465d5f5
S <s>
2023-12-07 17:44:13 +0400

    {"types":"C","runs":1}

--- Problem56.java
+++ Problem56.java
@@ -1,13 +1,29 @@
 import acm.program.ConsoleProgram;
 
 public class Problem56 extends ConsoleProgram {
+	private int n;
+	private int min;
 	
 	public void run() {
-		int n = readInt("? ");
+		n = readInt("? ");
 		int[] arr = new int[n];
+		min = 0;
 		for(int i = 0; i < n; i++){
 			arr[i] = readInt("Enter a number: ");
 		}
+		sortArray(arr);
+		
+	}
+
+	private void sortArray(int[] arr) {
+		for(int l = 0; l < n; l++){
+			min = arr[l];
+			if(arr[l + 1] < min){
+				min = arr[l + 1];
+			}
+		}
+		println(min);
+		
 	}
 
 }
