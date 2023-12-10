aba3630976befe61be3bc7a91d0873184735fc7d
S <s>
2023-12-10 21:39:11 +0400

    {"types":"C","runs":90}

--- testAgain.java
+++ testAgain.java
@@ -1,7 +1,12 @@
+import acm.graphics.GLabel;
 import acm.program.ConsoleProgram;
 
 import java.util.*;
 
 public class testAgain extends ConsoleProgram {
-
+	remove(wordLabel);
+	remove(incorrect);
+	GLabel lost = new GLabel("YOU LOST");
+	lost.setFont("Helvetic-26");
+	add(lost, getWidth()/2 - lost.getWidth()/2, 80 + SCAFFOLD_HEIGHT);
 }
