29086f63aa8f7ac2748fda5791adb2c6c85f11df
S <s>
2023-12-12 17:34:38 +0400

    {"types":"C","runs":2}

--- Problem60.java
+++ Problem60.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
-public class Problem60 {
+import acm.program.ConsoleProgram;
+
+public class Problem60 extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
+	public void run() {
+		
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		while(true){
+			int n = readInt("? ");
+			if(n == SENTINEL){
+				break;
+			}
+		}
+	}
 
 }
