9719fc66829828c25d22514c8e26d0e2bde4921a
S <s>
2023-12-12 16:49:38 +0400

    {"types":"C","runs":0}

--- Problem52.java
+++ Problem52.java
@@ -17,7 +17,7 @@ public class Problem52 extends ConsoleProgram {
 				}
 				println(line);
 			}
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
