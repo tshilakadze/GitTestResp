d1298ae10d8352ee78689c0d5eeb9a9ef8dc8feb
S <s>
2023-12-12 16:49:51 +0400

    {"types":"C","runs":0}

--- Problem52.java
+++ Problem52.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 import java.io.FileReader;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 
 public class Problem52 extends ConsoleProgram {
