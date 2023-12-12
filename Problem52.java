7ed8fe830d5becad13b3edb0baadc0d12321e3cc
S <s>
2023-12-12 16:47:06 +0400

    {"types":"C","runs":0}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem52 {
+import java.io.*;
+
+import java.io.FileReader;
+
+public class Problem52 extends ConsoleProgram {
+	public void run() {
+		BufferedReader rd = new BufferedReader(new FileReader("Problem52.java"));
+	}
 
 }
