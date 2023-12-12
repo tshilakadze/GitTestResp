548850f8f7d46e549819165b28a950a5f2608cca
S <s>
2023-12-12 17:59:44 +0400

    {"types":"C","runs":7}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Problem58 {
+public class Problem58 extends ConsoleProgram {
+	public void run() {
+		
+	}
+	
+	private boolean isMagicalMatrix(int [][] matrix){
+		
+		return false;
+	}
 
 }
