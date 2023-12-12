96e1916e6337ef1464278524116bc323aa220355
S <s>
2023-12-12 16:49:05 +0400

    {"types":"C","runs":0}

--- Problem52.java
+++ Problem52.java
@@ -1,12 +1,26 @@
 import acm.program.ConsoleProgram;
 
-import java.io.*;
-
 import java.io.FileReader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Problem52 extends ConsoleProgram {
 	public void run() {
-		BufferedReader rd = new BufferedReader(new FileReader("Problem52.java"));
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader("Problem52.java"));
+			while(true){
+				String line = rd.readLine();
+				if(line == null){
+					break;
+				}
+				println(line);
+			}
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
