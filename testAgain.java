fc17274f55de494a94386a41d5d159a88624e21a
S <s>
2023-12-14 23:23:58 +0400

    {"types":"C","runs":13}

--- testAgain.java
+++ testAgain.java
@@ -1,11 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 import acm.util.*;
 
 public class testAgain {
 	try {
 		BufferedReader rd = new BufferedReader(new FileReader("Problem54.java"));
+	} catch (IOException e) {
+		
+		e.printStackTrace();
 	}
 	
 }
