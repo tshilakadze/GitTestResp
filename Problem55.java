269a6199747444ea99cfd31cbe9f753b57490000
S <s>
2023-12-14 16:25:03 +0400

    {"types":"C","runs":5}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem55 {
+public class Problem55 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter a number: ");
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++){
+			arr[i] = readInt("? ");
+		}
+	}
 
 }
