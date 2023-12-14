8029b314c0bc7f0307f2199b493a7e2c4d7f7b39
S <s>
2023-12-14 16:39:25 +0400

    {"types":"C","runs":5}

--- Problem55.java
+++ Problem55.java
@@ -6,7 +6,22 @@ public class Problem55 extends ConsoleProgram{
 		int[] arr = new int[n];
 		for(int i = 0; i < n; i++){
 			arr[i] = readInt("? ");
+			if(isSorted(arr)){
+				println("sorted");
+			}
+			else{
+				println("not sorted");
+			}
 		}
+	}
+
+	private boolean isSorted(int[] arr) {
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] < arr[i - 1]){
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
