1872e2a357a284b26eef206ec6c43f5a1bbfe472
S <s>
2023-12-24 21:26:33 +0400

    {"types":"A","runs":5}

---
+++ testAgain.java
@@ -0,0 +1,15 @@
+import acm.program.*;
+import acm.util.RandomGenerator;
+
+public class testAgain extends ConsoleProgram{
+	
+	private RandomGenerator rgen = new RandomGenerator();
+	
+	public void run() {
+		int[] arr = new int[5];
+		for(int i = 0; i < arr.length; i++){
+			int num = rgen.nextInt(1, 6);
+			arr[i] = num;
+		}
+	}
+}
