b1c0a5ac862a1ce6dcd77255b312e651862f56d2
S <s>
2023-12-24 21:39:30 +0400

    {"types":"C","runs":8}

--- testAgain.java
+++ testAgain.java
@@ -11,8 +11,13 @@ public class testAgain extends ConsoleProgram{
 		int[] arr = new int[5];
 		for(int i = 0; i < arr.length; i++){
 			int num = rgen.nextInt(1, 6);
-			arr[i] = num;
+//			arr[i] = num;
 		}
+		arr[0] = 1;
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 2;
+		arr[4] = 2;
 		println(Arrays.toString(arr));
 		Arrays.sort(arr);
 		println(Arrays.toString(arr));
