5fed749aedee535a7b6dec3d5f227805a8133ba9
S <s>
2023-12-24 23:06:41 +0400

    {"types":"C","runs":27}

--- testAgain.java
+++ testAgain.java
@@ -8,6 +8,7 @@ public class testAgain extends ConsoleProgram{
 	private RandomGenerator rgen = new RandomGenerator();
 	
 	public void run() {
+		int sumOfDice = 0;
 		int[] arr = new int[5];
 		for(int i = 0; i < arr.length; i++){
 			int num = rgen.nextInt(1, 6);
@@ -18,7 +19,9 @@ public class testAgain extends ConsoleProgram{
 		arr[2] = 2;
 		arr[3] = 1;
 		arr[4] = 2;		*/
+		
 		println(Arrays.toString(arr));
+		println(sumOfDice);
 		Arrays.sort(arr);
 		println(Arrays.toString(arr));
 	}
