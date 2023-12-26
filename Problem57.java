ca7527b6b635ad67a42167b35b95ce1381871b0a
S <s>
2023-12-26 16:53:56 +0400

    {"types":"C","runs":11}

--- Problem57.java
+++ Problem57.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class Problem57 {
+public class Problem57 extends ConsoleProgram {
+	public void run() {
+		String text1 = readLine("First string: ");
+		String text2 = readLine("Second string: ");
+		println(anagramsOrNot(text1, text2));
+	}
+	
+	private boolean anagramsOrNot(String text1, String text2){
+		
+		return true;
+	}
 
 }
