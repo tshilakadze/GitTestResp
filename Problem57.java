f42667033f7bc3d427a314f49e35bece7cd785c9
S <s>
2023-12-26 17:21:11 +0400

    {"types":"C","runs":0}

--- Problem57.java
+++ Problem57.java
@@ -11,6 +11,24 @@ public class Problem57 extends ConsoleProgram {
 		if(text1.length() != text2.length()){
 			return false;
 		}
+		String chToString1 = "";
+		String chToString2 = "";
+		String[] arr1 = new String[text1.length()];
+		String[] arr2 = new String[text2.length()];
+		for(int i = 0; i < text1.length(); i++){
+			char ch = text1.charAt(i);
+			chToString1 = ch + "";
+			arr1[i] = chToString1;
+		}
+		for(int i = 0; i < text2.length(); i++){
+			char ch = text1.charAt(i);
+			chToString2 = ch + "";
+			arr2[i] = chToString2;
+		}
+		
+		if(!arr1.equals(arr2)){
+			return false;
+		}
 		
 		return true;
 	}
