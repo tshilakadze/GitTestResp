9975713c06685cba95724b49099fc451f948f3cb
S <s>
2024-01-03 23:52:48 +0400

    {"types":"C","runs":82}

--- Problem60.java
+++ Problem60.java
@@ -7,16 +7,6 @@ public class Problem60 extends ConsoleProgram{
 	private static final int SENTINEL = -1;
 	
 	public void run() {
-		
-		ArrayList<Integer> myList = new ArrayList<Integer>();
-		while(true){
-			int n = readInt("? ");
-			if(n == SENTINEL){
-				break;
-			}
-			myList.add(n);
-		}
-		println(myList);
 	}
 
 }
