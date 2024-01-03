82adab7cbce3edfb4efcf54cb4c3b0de56da0add
S <s>
2024-01-03 21:06:15 +0400

    {"types":"C","runs":34}

--- testAgain.java
+++ testAgain.java
@@ -8,7 +8,9 @@ public class testAgain extends ConsoleProgram{
 		for(int i = 0; i < arr.length; i++){
 			for(int l = 0; l < arr[0].length; l++){
 				arr[i][l] = 1;
+				print(arr[i][l]);
 			}
+			println();
 		}
 		for(int i = 0; i < arr.length; i++){
 			for(int l = 0; l < arr[0].length; l++){
