4e03cf76d5e0fbe7a0d0f7e217d66a00446778eb
S <s>
2024-01-03 19:53:16 +0400

    {"types":"C","runs":24}

--- testAgain.java
+++ testAgain.java
@@ -4,22 +4,7 @@ import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram{
 	public void run(){
-		String a = readLine("? ");
-		String b = readLine("? ");
-		char[] c = new char[a.length()];
-		char[] d = new char[b.length()];
-		for(int i = 0; i < a.length(); i++){
-			c[i] = a.charAt(i);
-		}
-		for(int i = 0; i < b.length(); i++){
-			d[i] = b.charAt(i);
-		}
-		Arrays.sort(c);
-		Arrays.sort(d);
-		println(Arrays.toString(c));
-		println(Arrays.toString(d));
-		if(c == d){
-			println("true");
-		}
+		int[] arr = new int[4];
+		println(Arrays.toString(arr));
 	}
 }
