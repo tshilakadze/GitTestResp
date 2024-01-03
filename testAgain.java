623206189351cc561258cef3550ac28da9ba4b7e
S <s>
2024-01-03 20:19:26 +0400

    {"types":"C","runs":28}

--- testAgain.java
+++ testAgain.java
@@ -5,9 +5,10 @@ public class testAgain extends ConsoleProgram{
 		int[][] arr = new int[4][4];
 		for(int i = 0; i < arr.length; i++){
 			for(int l = 0; l < arr[0].length; l++){
-				print(arr[i][l] + "");
+				print(arr[i][l]);
 			}
 			println();
 		}
+		
 	}
 }
