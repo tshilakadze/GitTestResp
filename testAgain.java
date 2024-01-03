cb7848b3dbafdd10ffe595ad6bb8edbe7d70a4ea
S <s>
2024-01-03 21:03:56 +0400

    {"types":"C","runs":31}

--- testAgain.java
+++ testAgain.java
@@ -5,6 +5,7 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram{
 	public void run(){
 		int[][] arr = new int[4][5];
+		arr[2][4] = 23;
 		for(int i = 0; i < arr.length; i++){
 			for(int l = 0; l < arr[0].length; l++){
 				print(arr[i][l]);
