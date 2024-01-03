1c65b64cca7862f47724257e765e77b559166d35
S <s>
2024-01-03 21:42:24 +0400

    {"types":"C","runs":41}

--- testAgain.java
+++ testAgain.java
@@ -7,7 +7,7 @@ public class testAgain extends ConsoleProgram{
 		int[][] arr = new int[4][5];
 		for(int i = 0; i < arr.length; i++){
 			for(int l = 0; l < arr[0].length; l++){
-				arr[i][l] = 2;
+				arr[i][l] = l + 1;
 				print(arr[i][l]);
 			}
 			println();
@@ -16,7 +16,7 @@ public class testAgain extends ConsoleProgram{
 		for(int i = 0; i < arr2.length; i++){
 			for(int r = 0; r < arr.length; r++){
 				for(int c = 0; c < arr[0].length; c++){
-					arr2[i] = arr[r][c] + 1;
+					arr2[i] = arr[r][c];
 				}
 			}
 		}
