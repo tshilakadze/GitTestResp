cf4740f064135993a494f817407652857f8138c6
S <s>
2024-01-03 23:13:24 +0400

    {"types":"C","runs":48}

--- testAgain.java
+++ testAgain.java
@@ -2,23 +2,22 @@ import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class testAgain extends ConsoleProgram{
-	public void run(){
+public class testAgain extends ConsoleProgram {
+	public void run() {
 		int[][] arr = new int[2][3];
-		for(int i = 0; i < arr.length; i++){
-			for(int l = 0; l < arr[0].length; l++){
+		for (int i = 0; i < arr.length; i++) {
+			for (int l = 0; l < arr[0].length; l++) {
 				arr[i][l] = readInt("? ");
 				print(arr[i][l]);
 			}
 			println();
 		}
 		int[] arr2 = new int[6];
-		
-		for(int i = 0; i < arr2.length; i++){
-			for(int r = 0; r < arr.length; r++){
-				for(int c = 0; c < arr[0].length; c++){
-					arr2[i] = arr[r][c];
-				}
+		int count = 0;
+		for (int r = 0; r < arr.length; r++) {
+			for (int c = 0; c < arr[0].length; c++) {
+				arr2[count] = arr[r][c];
+				count++;
 			}
 		}
 		println(Arrays.toString(arr2));
