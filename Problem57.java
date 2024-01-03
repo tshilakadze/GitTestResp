3ab17f5798665fbee68bf496d74c6a9ca40aa868
S <s>
2024-01-03 16:06:56 +0400

    {"types":"C","runs":13}

--- Problem57.java
+++ Problem57.java
@@ -20,7 +20,14 @@ public class Problem57 extends ConsoleProgram {
 		}
 		Arrays.sort(first);
 		Arrays.sort(second);
-		
+		for(int i = 0; i < text1.length(); i++){
+			if(first[i] != second[i]){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
 		return false;
 	}
 
