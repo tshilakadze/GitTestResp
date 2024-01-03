12a884b37cdecfc788d5a120ed102e30fa339f06
S <s>
2024-01-03 15:46:56 +0400

    {"types":"C","runs":3}

--- testAgain.java
+++ testAgain.java
@@ -5,7 +5,7 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram{
 	public void run(){
 		String a = readLine("? ");
-//		String b = readLine("? ");
+		String b = readLine("? ");
 		char[] c = new char[a.length()];
 		for(int i = 0; i < a.length(); i++){
 			c[i] = a.charAt(i);
