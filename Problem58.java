c74db8e4b710a06567d51fb8e975f6b841ef4618
S <s>
2024-01-03 19:27:29 +0400

    {"types":"C","runs":24}

--- Problem58.java
+++ Problem58.java
@@ -4,11 +4,7 @@ import acm.program.ConsoleProgram;
 
 public class Problem58 extends ConsoleProgram {
 	public void run() {
-		int m = readInt("? ");
-		int n = readInt("? ");
-		int[][] myMatrix = new int[m][n];
-		println(Arrays.toString(myMatrix));
-		println(isMagicalMatrix(myMatrix));
+		
 	}
 	
 	private boolean isMagicalMatrix(int [][] matrix){
