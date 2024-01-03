0f574ec60dcb7beefe6865d6fce93518826f9a27
S <s>
2024-01-03 15:53:17 +0400

    {"types":"C","runs":5}

--- testAgain.java
+++ testAgain.java
@@ -7,10 +7,15 @@ public class testAgain extends ConsoleProgram{
 		String a = readLine("? ");
 		String b = readLine("? ");
 		char[] c = new char[a.length()];
+		char[] d = new char[b.length()];
 		for(int i = 0; i < a.length(); i++){
 			c[i] = a.charAt(i);
 		}
+		for(int i = 0; i < b.length(); i++){
+			d[i] = b.charAt(i);
+		}
 		Arrays.sort(c);
+		Arrays.sort(d);
 		println(Arrays.toString(c));
 	}
 }
