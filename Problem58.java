9d11b3826439fd312477b82f5714ef0f6552191a
S <s>
2024-01-03 23:36:27 +0400

    {"types":"C","runs":57}

--- Problem58.java
+++ Problem58.java
@@ -6,8 +6,8 @@ public class Problem58 extends ConsoleProgram {
 	public void run() {
 		int[][] matrix1 = {
 				{1, 2, 3, 4},
-				{9, 7, 8},
-				{4, 5, 6}};
+				{9, 7, 8, 4},
+				{4, 5, 6, 4}};
 		println(isMagicalMatrix(matrix1));
 	}
 	
