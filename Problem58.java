690d01c11c0864952c3a5e19781a154d18ce0173
S <s>
2024-01-03 19:53:23 +0400

    {"types":"C","runs":25}

--- Problem58.java
+++ Problem58.java
@@ -8,7 +8,11 @@ public class Problem58 extends ConsoleProgram {
 	}
 	
 	private boolean isMagicalMatrix(int [][] matrix){
-		
+		if(matrix.length != matrix[0].length){
+			return false;
+		}
+		int ar = matrix.length;
+		int[] allNums = new int[ar*ar];
 		return false;
 	}
 
