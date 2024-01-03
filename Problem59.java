bb2c7ab3f1da614599bd306e59d798cbace057ab
S <s>
2024-01-04 02:55:32 +0400

    {"types":"C","runs":82}

--- Problem59.java
+++ Problem59.java
@@ -17,8 +17,8 @@ public class Problem59 extends ConsoleProgram {
 		int maxSize = 0;
 		for(int r1 = 0; r1 < matrix.length; r1++){
 			for(int c1 = 0; c1 < matrix[0].length; c1++){
-				for(int r2 = 0; r2 < matrix.length; r2++){
-					for(int c2 = 0; c2 < matrix[0].length; c2++){
+				for(int r2 = r1; r2 < matrix.length; r2++){
+					for(int c2 = c1; c2 < matrix[0].length; c2++){
 						if(justOnes(matrix, r1, c1, r2, c2)){
 							int size = (r2 - r1 + 1)*(c2 - c1 + 1);
 							maxSize = Math.max(maxSize, size);
@@ -32,8 +32,8 @@ public class Problem59 extends ConsoleProgram {
 
 	private boolean justOnes(int[][] matrix, int r1, int c1, int r2, int c2) {
 		int count = 0;
-		for(int i = r1; i < r2; i++){
-			for(int j = c1; j < c2; j++){
+		for(int i = r1; i <= r2; i++){
+			for(int j = c1; j <= c2; j++){
 				
 			}
 		}
