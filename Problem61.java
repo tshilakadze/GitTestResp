3548a43bdcdfcb3d651246e12c573d953310a65f
S <s>
2024-01-04 12:44:05 +0400

    {"types":"C","runs":5}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class Problem61 {
+public class Problem61 extends ConsoleProgram {
+	public void run() {
+		int m = readInt("Enter m: ");
+		int n = readInt("Enter n: ");
+		
+	}
 
 }
