fc3b9c60a64a3f9d6cc1d4c556921aa59bd233fa
S <s>
2024-01-04 19:01:48 +0400

    {"types":"C","runs":36}

--- testAgain.java
+++ testAgain.java
@@ -4,19 +4,6 @@ import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
 	public void run() {
-		Map<String, Integer> map = new HashMap<>();
-		while (true) {
-			String name = readLine("Enter name: ");
-			if (name.equals("")) {
-				break;
-			}
-			if (!map.containsKey(name)) {
-				map.put(name, 0);
-			}
-			map.put(name, map.get(name) + 1);
-		}
-		for(String name : map.keySet()){
-			println(name + " - " + map.get(name));
-		}
+		
 	}
 }
