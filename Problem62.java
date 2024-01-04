c4963dfdd94acc3c71bb9e80ba036c3917f22c1e
S <s>
2024-01-04 14:42:37 +0400

    {"types":"C","runs":17}

--- Problem62.java
+++ Problem62.java
@@ -20,8 +20,22 @@ public class Problem62 extends ConsoleProgram {
 		ArrayList<String> myList = new ArrayList<String>();
 		for(int i = 0; i < names.size(); i++){
 			String currName = names.get(i);
-			
+			if(!myList.contains(currName)){
+				int num = countNames(names, currName);
+				println(currName + ": " + num);
+				myList.add(currName);
+			}
 		}
+	}
+
+	private int countNames(ArrayList<String> names, String currName) {
+		int numOfNames = 0;
+		for(int i = 0; i < names.size(); i++){
+			if(names.get(i).equals(currName)){
+				numOfNames += 1;
+			}
+		}
+		return numOfNames;
 	}
 
 }
