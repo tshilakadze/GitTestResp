2676aefd974d2c29651df205cbd593cf4be1cf84
S <s>
2024-01-04 17:57:48 +0400

    {"types":"C","runs":18}

--- testAgain.java
+++ testAgain.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
 	public void run() {
+		HashMap<String, String> testMap = new HashMap<String, String>();
+		testMap.put("dog", "woof");
 		
 	}
 }
