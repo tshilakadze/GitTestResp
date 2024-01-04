56d3df679e70a1b45cd1ff527739d09872665c69
S <s>
2024-01-04 18:39:21 +0400

    {"types":"C","runs":24}

--- Problem63.java
+++ Problem63.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import acm.program.ConsoleProgram;
 
 public class Problem63 extends ConsoleProgram {
+	private String mostFollowed;
 	public void run() {
 		HashMap<String, String> followers = new HashMap<String, String>();
 		String person;
@@ -20,8 +21,21 @@ public class Problem63 extends ConsoleProgram {
 		}
 	}
 
-	private void personWithMostFollowers(HashMap<String, String> followers) {
-		
+	private String personWithMostFollowers(HashMap<String, String> followers) {
+		int mostFollowers = 0;
+		for(int i = 0; i < followers.size(); i++){
+			int currNum = countFollowers(followers, followers.get(i));
+			if(currNum > mostFollowers){
+				mostFollowers = currNum;
+				mostFollowed = followers.get(i);
+			}
+		}
+		return mostFollowed;
+	}
+
+	private int countFollowers(HashMap<String, String> followers, String string) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
