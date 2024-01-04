06b2fbbd6e7a04537f9a36c2b9afd57a5dae6e14
S <s>
2024-01-04 19:49:56 +0400

    {"types":"C","runs":41}

--- Problem63.java
+++ Problem63.java
@@ -11,25 +11,25 @@ import acm.program.ConsoleProgram;
 
 public class Problem63 extends ConsoleProgram {
 	public void run() {
-		Map<String, ArrayList<String>> followers = new HashMap<String, ArrayList<String>>();	
+		Map<String, ArrayList<String>> followersMap = new HashMap<String, ArrayList<String>>();	
 		while(true){
 			String follower = readLine("Enter follower's name: ");
 			if(follower.isEmpty()){
 				break;
 			}
 			String person = readLine("Enter person's name: ");
-			if(!followers.containsKey(person)){
-				followers.put(person, new ArrayList<String>());
+			if(!followersMap.containsKey(person)){
+				followersMap.put(person, new ArrayList<String>());
 			}
-			ArrayList<String> myFollowers = followers.get(person);
+			ArrayList<String> myFollowers = followersMap.get(person);
 			myFollowers.add(follower);
 		}
-		println(followers);		//Problem64
+	//	println(followersMap);		//Problem64
 		
 		int maxFollowers = 0;
 		String mostPopular = null;
-		for(String person: followers.keySet()){
-			int followerCount = followers.get(person).size();
+		for(String person: followersMap.keySet()){
+			int followerCount = followersMap.get(person).size();
 			if(followerCount > maxFollowers){
 				maxFollowers = followerCount;
 				mostPopular = person;
@@ -38,5 +38,6 @@ public class Problem63 extends ConsoleProgram {
 		println("Person with most followers is " + mostPopular);
 		
 		int count = 0;
+		
 	}
 }
