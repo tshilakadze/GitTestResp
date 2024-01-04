04aeff3698c9fdfd657bb2546f7567934ae21869
S <s>
2024-01-04 13:43:49 +0400

    {"types":"C","runs":6}

--- Problem61.java
+++ Problem61.java
@@ -11,6 +11,13 @@ public class Problem61 extends ConsoleProgram {
 		for(int i = 0; i < n; i++){
 			myList.add(a = readInt("? "));
 		}
+		for(int i = 0; i < myList.size(); i++){
+			for(int l = 0; l < myList.size(); l++){
+				if(myList.get(i) + myList.get(l) == m){
+					println(myList.get(i) + " and " + myList.get(l));
+				}
+			}
+		}
 	}
 
 }
