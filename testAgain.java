88609c46a7bed0c3791e7c511c9f436fce860f09
S <s>
2024-01-04 12:42:18 +0400

    {"types":"C","runs":5}

--- testAgain.java
+++ testAgain.java
@@ -4,24 +4,7 @@ import java.util.Arrays;
 import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
-	
-	private static final int SENTINEL = -1;
-	
 	public void run() {
-		version1();
-	}
-	
-	private void version1() {
-		ArrayList<Integer> myList = new ArrayList<Integer>();
-		while(true) {
-			int n = readInt("Enter num: ");
-			if(n == SENTINEL) {
-				break;
-			}
-			myList.add(n);
-		}
-		for(int i = myList.size() - 1; i >= 0; i--) {
-			println(myList.get(i));
-		}
+		
 	}
 }
