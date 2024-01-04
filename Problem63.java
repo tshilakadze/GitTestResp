ea15f10110489bd53d9cf058ed4cd3004c7dee28
S <s>
2024-01-04 19:10:34 +0400

    {"types":"C","runs":36}

--- Problem63.java
+++ Problem63.java
@@ -8,12 +8,14 @@ import acm.program.ConsoleProgram;
 
 public class Problem63 extends ConsoleProgram {
 	public void run() {
-		Map<String, ArrayList<String>> followers = new HashMap<String, ArrayList<String>>();
-		String follower;
+		Map<String, ArrayList<String>> followers = new HashMap<String, ArrayList<String>>();	
 		while(true){
 			String person = readLine("Enter person's name: ");
 			if(person.isEmpty()){
 				break;
+			}
+			if(!followers.containsKey(person)){
+				followers.put(person, new ArrayList<String>());
 			}
 		}
 	}
