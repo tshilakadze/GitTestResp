25598567f6bc71c30ec028be75070cc7565a7a38
S <s>
2024-01-04 13:46:56 +0400

    {"types":"C","runs":12}

--- Problem61.java
+++ Problem61.java
@@ -6,19 +6,14 @@ public class Problem61 extends ConsoleProgram {
 	public void run() {
 		int m = readInt("Enter m: ");
 		int n = readInt("Enter n: ");
-		int a;
 		ArrayList<Integer> myList = new ArrayList<Integer>();
 		for(int i = 0; i < n; i++){
-			myList.add(a = readInt("? "));
+			myList.add(readInt("? "));
 		}
 		for(int i = 0; i < myList.size(); i++){
 			for(int l = 0; l < myList.size(); l++){
 				if(myList.get(i) + myList.get(l) == m){
 					println(myList.get(i) + " and " + myList.get(l));
-				}
-				else{
-					println("There are no such numbers.");
-					break;
 				}
 			}
 		}
