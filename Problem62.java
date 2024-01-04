fcd5dac5340af9fb7d47e570182512e11e6d20f8
S <s>
2024-01-04 14:34:56 +0400

    {"types":"C","runs":17}

--- Problem62.java
+++ Problem62.java
@@ -6,12 +6,19 @@ public class Problem62 extends ConsoleProgram {
 	public void run() {
 		ArrayList<String> names = new ArrayList<String>();
 		String name;
+		int count = 0;
 		while(true){
 			names.add(name = readLine("? "));
 			if(name.equals("")){
+				names.remove(names.size() - 1);
+				countNames(names);
 				break;
 			}
 		}
+	}
+
+	private void countNames(ArrayList<String> names) {
+		
 	}
 
 }
