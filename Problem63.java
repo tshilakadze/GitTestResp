1182935028f9b893ae432e0f195ad2e4c861a894
S <s>
2024-01-04 19:15:12 +0400

    {"types":"C","runs":38}

--- Problem63.java
+++ Problem63.java
@@ -10,10 +10,11 @@ public class Problem63 extends ConsoleProgram {
 	public void run() {
 		Map<String, ArrayList<String>> followers = new HashMap<String, ArrayList<String>>();	
 		while(true){
-			String person = readLine("Enter person's name: ");
-			if(person.isEmpty()){
+			String follower = readLine("Enter follower's name: ");
+			if(follower.isEmpty()){
 				break;
 			}
+			String person = readLine("Enter person's name: ");
 			if(!followers.containsKey(person)){
 				followers.put(person, new ArrayList<String>());
 			}
