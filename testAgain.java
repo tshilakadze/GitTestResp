9e8a52c4c7c5ce88c7e14dd8a2e77fbfa1a29a47
S <s>
2024-01-04 19:46:25 +0400

    {"types":"C","runs":41}

--- testAgain.java
+++ testAgain.java
@@ -5,43 +5,6 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram {
 	private static final String SENTINEL = "";
 	public void run() {
-		Map<String, ArrayList<String>> friendLists = 
-				new HashMap<String, ArrayList<String>> ();
-//		gio -> [mari, ele, ale]
-		while(true) {
-			String friend = readLine("Enter name of a friend: ");
-			if(friend.equals(SENTINEL)) {
-				break;
-			}
-			String person = readLine("Enter name of a person: ");
-			
-			
-			if(!friendLists.containsKey(person)) {
-				friendLists.put(person, new ArrayList<String>());
-			}
-			
-			ArrayList<String> currFriends = friendLists.get(person);
-			currFriends.add(friend); // TODO add check for duplicates
-			
-			// we don't need to call put again
-//			friendLists.put(person, currFriends);
-			
-		}
-		
-	
-		println(friendLists);
-		
-		int maxFriends = 0;
-		String popularPerson = null;
-		
-		for(String person: friendLists.keySet()) {
-			int friendCount = friendLists.get(person).size();
-			if(friendCount > maxFriends) {
-				maxFriends = friendCount;
-				popularPerson = person;
-			}
-		}
-		
-		println("Person with most friends: " + popularPerson);		
+				
 	}
 }
