2aa3071c5f5a3078a089db209481a073a76aa8c1
S <s>
2024-01-04 18:29:47 +0400

    {"types":"C","runs":23}

--- Problem63.java
+++ Problem63.java
@@ -2,11 +2,18 @@
  * ცარიელ სტრინგს. დაბეჭდეთ იმ ადამიანის სახელი, ვისაც ჰყავს ყველაზე მეტი გამომწერი.
  */
 
+import java.util.HashMap;
+
 import acm.program.ConsoleProgram;
 
 public class Problem63 extends ConsoleProgram {
 	public void run() {
-		
+		HashMap<String, String> followers = new HashMap<String, String>();
+		String person;
+		String follower;
+		while(true){
+			followers.put(person = readLine("key: "), follower = readLine("value: "));
+		}
 	}
 
 }
