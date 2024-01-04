2dc6e9e01a624bd5527ecb53d5f926afb46b31d7
S <s>
2024-01-04 13:37:27 +0400

    {"types":"C","runs":5}

--- Problem61.java
+++ Problem61.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 
 public class Problem61 extends ConsoleProgram {
 	public void run() {
 		int m = readInt("Enter m: ");
 		int n = readInt("Enter n: ");
-		
+		int a;
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		for(int i = 0; i < n; i++){
+			myList.add(a = readInt("? "));
+		}
 	}
 
 }
