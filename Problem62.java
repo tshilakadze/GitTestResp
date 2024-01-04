15bebf8fac29e9345339d1c5f68b6742e83ba0bf
S <s>
2024-01-04 13:55:13 +0400

    {"types":"C","runs":15}

--- Problem62.java
+++ Problem62.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
-public class Problem62 {
+import acm.program.*;
+
+public class Problem62 extends ConsoleProgram {
+	public void run() {
+		ArrayList<String> names = new ArrayList<String>();
+		String name;
+		while(true){
+			names.add(name = readLine("? "));
+			if(name == ""){
+				break;
+			}
+		}
+	}
 
 }
