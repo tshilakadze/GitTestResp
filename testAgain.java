bd8382efdaec680bb6caa1f8fab71e062d747a14
S <s>
2024-01-04 19:46:30 +0400

    {"types":"C","runs":41}

--- testAgain.java
+++ testAgain.java
@@ -3,7 +3,7 @@ import java.util.*;
 import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
-	private static final String SENTINEL = "";
+//	private static final String SENTINEL = "";
 	public void run() {
 				
 	}
