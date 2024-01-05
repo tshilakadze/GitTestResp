8040a9e758e1051a0ab0b5bab492eaf7233be36f
S <s>
2024-01-05 15:30:44 +0400

    {"types":"C","runs":6}

--- testAgain.java
+++ testAgain.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
+import javax.swing.JTextField;
+
 import acm.program.ConsoleProgram;
 
 public class testAgain extends ConsoleProgram {
 //	private static final String SENTINEL = "";
 	public void run() {
-				
+		JTextField field = new JTextField(10);
+		add(field, SOUTH);
 	}
 }
