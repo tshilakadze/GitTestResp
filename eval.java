6f6d16adca61f9f8658c2a00fe5a6dbd5d9bbc1e
S <s>
2024-01-05 21:29:57 +0400

    {"types":"C","runs":27}

--- eval.java
+++ eval.java
@@ -2,26 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class eval extends ConsoleProgram{
 	public void run(){
-		String s = "2  + 5 - 3";
-		System.out.println(eval(s));
+	
 	}
-	private int eval(String s){
-		s.replaceAll(" ", "");
-		int result = 0;
-		for(int i = 0; i < s.length(); i++){
-			if(i == 0){
-				result = s.charAt(0) - '0';
-			}
-			if(s.charAt(i) == '+'){
-				result = result + (s.charAt(i) - '0');
-			}
-			if(s.charAt(i) == '-'){
-				result = result - (s.charAt(i) - '0');
-			}
-			if(s.length() == 1){
-				return s.charAt(0) - '0';
-			}
-		}
-		return result;
 	}
 }
