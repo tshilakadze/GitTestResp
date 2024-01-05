530d9a078264bfa7a3dd48ef2ffff067f8afb800
S <s>
2024-01-05 17:43:49 +0400

    {"types":"C","runs":13}

--- testAgain.java
+++ testAgain.java
@@ -7,6 +7,10 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram {
 //	private static final String SENTINEL = "";
 	public void run() {
+		
+		JButton redButton = new JButton("Red");
+		
+		
 /*		JTextField field = new JTextField(5);
 		add(field, SOUTH);
 		field.setText("rugby");
