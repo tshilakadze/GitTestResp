aa93e9acc302f80e1305c281edebb623e1676876
S <s>
2024-01-05 21:32:23 +0400

    {"types":"C","runs":27}

--- eval.java
+++ eval.java
@@ -2,6 +2,32 @@ import acm.program.ConsoleProgram;
 
 public class eval extends ConsoleProgram{
 	public void run(){
+		String str = readLine("? ");
+		int answ = eval(str);
+	}
+
+	private int eval(String str) {
+		int result = 0;
+		for(int i = 0; i < str.length(); i++){
+			char ch = str.charAt(i);
+			if(ch == '-'){
+				String part = str.substring(0, i - 1);
+				str = str.substring(i + 2);
+				int num = stringToInt(part);
+				result = result - num;
+			}
+			if(ch == '+'){
+				String part1 = str.substring(0, i - 1);
+				str = str.substring(i + 2);
+				int num1 = stringToInt(part1);
+				result = result + num1;
+			}
+		}
+		return result;
+	}
 	
+	private int stringToInt(String str){
+		int x = Integer.parseInt(str);
+		return x;
 	}
 }
