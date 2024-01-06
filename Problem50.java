08e95953ae73b62e3df9250fcbc6398fe06419e8
S <s>
2024-01-06 14:30:45 +0400

    {"types":"C","runs":4}

--- Problem50.java
+++ Problem50.java
@@ -9,9 +9,9 @@ public class Problem50 extends ConsoleProgram {
 		StringTokenizer tokenizer = new StringTokenizer(text);
 		while(tokenizer.hasMoreTokens()){
 			tokenizer.nextToken();
+			println(tokenizer.nextElement());
 			numOfWords += 1;
 		}
-		println(tokenizer.nextToken());
 		println("There are " + numOfWords + " words in this string.");
 	}
 }
