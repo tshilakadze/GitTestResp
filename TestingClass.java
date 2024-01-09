c549b0afab9d4b0f76849e65c7da3a305dbfd53c
S <s>
2024-01-09 17:36:31 +0400

    {"types":"C","runs":3}

--- TestingClass.java
+++ TestingClass.java
@@ -11,6 +11,7 @@ public class TestingClass extends ConsoleProgram {
 //		TestClass obj = new TestClass();
 		TestClass obj = new TestClass(students);
 		println(obj.getScore("mberi15"));
+		println(obj.getScore("mberi5"));
 	}
 
 }
