816bb2e98fb81bf2c20822dce0cd6213506f1eca
S <s>
2024-01-09 17:18:50 +0400

    {"types":"C","runs":2}

--- TestClass.java
+++ TestClass.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
 public class TestClass {
+	private Map<String, Double> studentScores;
 	
-	public TestClass(ArrayList<String> students){
+	
+	public TestClass(){
 		
 		
 	}
 
+	public TestClass(ArrayList<String> students) {
+		
+	}
 }
