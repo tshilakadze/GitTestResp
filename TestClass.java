e872b2e53f7df99898ce59080b435dd69021ec60
S <s>
2024-01-09 17:31:02 +0400

    {"types":"C","runs":2}

--- TestClass.java
+++ TestClass.java
@@ -5,11 +5,23 @@ public class TestClass {
 	
 	
 	public TestClass(){
-		
+		studentScores = new HashMap<String, Double>();
 		
 	}
 
 	public TestClass(ArrayList<String> students) {
-		
+		studentScores = new HashMap<String, Double>();
+		for(String student: students){		// = for(int i = 0; i < students.size(); i++){String student = students.get(i);}
+			studentScores.put(student,  null);
+		}
+//		studentScores.put(null, null);
+	}
+	
+	public Double getScore(String student){
+		if(!studentScores.containsKey(student)){
+			System.out.println("No such student: " + student)
+				return null;
+			}
+		return studentScores.get(student);
 	}
 }
