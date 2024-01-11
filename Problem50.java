76f54c6383c57ec19e1db92fac265ca1deda6373
S <s>
2024-01-12 02:06:36 +0400

    {"types":"C","runs":53}

--- Problem50.java
+++ Problem50.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Problem50 extends ConsoleProgram {
 	public void run() {
 		int numOfWords = 0;
+		int num = 0;
+		int[] nums = new int[20];
 		String text = readLine("Enter the text: ");
 		StringTokenizer tokenizer = new StringTokenizer(text);
 		while(tokenizer.hasMoreTokens()){
