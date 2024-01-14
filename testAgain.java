a5c1c7a7dd13cbce56a7943ad326fd008a4af2f0
S <s>
2024-01-14 18:31:40 +0400

    {"types":"A","runs":0}

---
+++ testAgain.java
@@ -0,0 +1,29 @@
+import java.awt.event.ActionEvent;
+import java.util.*;
+
+import javax.swing.*;
+
+import acm.program.ConsoleProgram;
+
+public class testAgain extends ConsoleProgram {
+//	private static final String SENTINEL = "";
+	public void run() {
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		for(int i = 5; i > 0; i--){
+			nums.add(i);
+		}
+		println(nums.toString());
+		println(nums.get(4));
+	}
+	
+}
+
+
+//		555 244 676
+
+
+
+
+
+
+
