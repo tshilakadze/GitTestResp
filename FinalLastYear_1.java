da5b214da418ba27aaeea0dd0969cf7136563ad0
S <s>
2024-01-20 13:53:21 +0400

    {"types":"C","runs":10}

--- FinalLastYear_1.java
+++ FinalLastYear_1.java
@@ -21,7 +21,7 @@ public class FinalLastYear_1 extends ConsoleProgram {
 				num = part1.length();
 				String numStr = num + "";
 				line = part1.charAt(0) + numStr;
-				
+				text = text.substring(i);
 			}
 		}
 		return line;
