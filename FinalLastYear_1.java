a7a38fc2cc1d4c68f356c1a4f377f667846fba0e
S <s>
2024-01-20 13:10:02 +0400

    {"types":"C","runs":0}

--- FinalLastYear_1.java
+++ FinalLastYear_1.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class FinalLastYear_1 {
+public class FinalLastYear_1 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter String: ");
+		compressString(text);
+	}
+
+	private void compressString(String text) {
+		
+	}
 
 }
