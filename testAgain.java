0dc8d66ebb523643697d39392792add1f9445a08
S <s>
2024-01-21 18:55:56 +0400

    {"types":"C","runs":69}

--- testAgain.java
+++ testAgain.java
@@ -8,10 +8,10 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram {
 //	private static final String SENTINEL = "";
 	public void run() {
-		Map<String, String> bla = new HashMap<String, String>();
-		bla.put("key1", "value2");
-		bla.put("key2", "value2");
-		println(bla.size());
+		ArrayList<String> bla = new ArrayList<String>();
+		bla.add("bla bla bla");
+		bla.add("nfncimeci");
+		println(bla.toString());
 	}
 	
 }
