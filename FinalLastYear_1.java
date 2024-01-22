e72208ce1e981ad6b781512b3644fe9a0d800ec4
S <s>
2024-01-22 14:28:38 +0400

    {"types":"C","runs":0}

--- FinalLastYear_1.java
+++ FinalLastYear_1.java
@@ -1,10 +1,21 @@
+//ამოცანა 1. (15 ქულა) სტრინგების კომპრესირება
+//დაწერეთ მეთოდი, რომელსაც გადაეცემა სტრინგი და აბრუნებს ამ სტრინგის
+//კომპრესირებულ ვერსიას შემდეგი წესით: ნაცვლად ერთმანეთის მიმდევრობით
+//გამეორებული სიმბოლოებისა წერს სიმბოლოს და შემდეგ მის რაოდენობას.
+//მაგალითად:
+//“aabcccccaa” -> “a2b1c5a2”.
+//“wwwaaaage” -> “w3a4g1e1”
+//იმ შემთხვევაში თუ ამ წესით კომპრესირებული სტრინგი უფრო გრძელია ვიდრე
+//საწყისი, მეთოდმა უნდა დააბრუნოს საწყისი სტრინგი უცვლელად. მაგალითად:
+//“abc”-ს კომპრესირებული ვერსიაა “a1b1c1”, რომელიც თავდაპირველზე უფრო
+//გრძელია, ამიტომ მეთოდი დააბრუნებს “abc”.
+
 import acm.program.ConsoleProgram;
 
 public class FinalLastYear_1 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter String: ");
-		String result = compressedString(text);
-		println(result);
+		println(compressedString(text));
 	}
 
 	private String compressedString(String text) {
