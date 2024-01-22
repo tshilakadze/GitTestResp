cc2f7a0dbf3a38bf056c81420fc866a0aaf63b64
S <s>
2024-01-22 17:39:05 +0400

    {"types":"C","runs":42}

--- FinalLastYear_2.java
+++ FinalLastYear_2.java
@@ -65,7 +65,7 @@ public class FinalLastYear_2 extends ConsoleProgram {
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		while(true){
 			int num = rgen.nextInt();
-			if(!contains(mat, num) && num != 0){
+			if(!contains(mat, num)){
 				return num;
 			}
 		}
