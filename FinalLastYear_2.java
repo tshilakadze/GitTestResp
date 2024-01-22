fbf99a40577a4dc20308bd3831d17ece068ceead
S <s>
2024-01-22 18:58:38 +0400

    {"types":"C","runs":54}

--- FinalLastYear_2.java
+++ FinalLastYear_2.java
@@ -58,11 +58,25 @@ public class FinalLastYear_2 extends ConsoleProgram {
 	}
 
 	private void changeRowNums(int[][] mat, int r) {
-		for(int i = 0; i < mat[r].length; i++){
-			mat[r][i] = tmp;
+		for(int r1 = 0; r1 < mat.length; r1++){
+			for(int i = 0; i < mat[r].length; i++){
+				mat[r][i] = tmp;
+			}
 		}
+		
+		/*for(int r1 = 0; r1 < mat.length; r1++){
+			for(int c = 0; c < mat[0].length; c++){
+				if(mat[r1][c] == 0 && r1 != r){
+					mat[r]
+				}
+			}
+		}*/
 	}
 
+	
+	
+	
+	
 	private int tempNum(int[][] mat) {
 		for(int i = 0; i <= mat.length * mat[0].length; i++){
 				if(!contains(mat, i)){
