7f87ececc698169a03620e7856f50245f373ffdf
S <s>
2024-01-23 19:09:17 +0400

    {"types":"C","runs":64}

--- FinalLastYear_4.java
+++ FinalLastYear_4.java
@@ -87,7 +87,7 @@ public class FinalLastYear_4 extends GraphicsProgram{
 
 	private void displayRow(ArrayList<String> rowWords) {
 		int x = COLUMN_START_X;
-		int y = COLUMN_START_Y;
+		int y = COLUMN_START_Y + ROW_STEP;
 		for(int i = 0; i < rowWords.size(); i++){
 			GLabel theWord = new GLabel(rowWords.get(i));
 			x = x + COLUMN_STEP*i;
