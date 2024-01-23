d8e5900a906ebb28cd65ff878fa70cd4bb1e9a36
S <s>
2024-01-23 15:40:19 +0400

    {"types":"C","runs":22}

--- FinalLastYear_4.java
+++ FinalLastYear_4.java
@@ -1,4 +1,21 @@
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class FinalLastYear_4 {
+import javax.swing.*;
 
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class FinalLastYear_4 extends GraphicsProgram{
+	
+	private static final int COLUMN_START_X = 10;
+	private static final int COLUMN_START_Y = 20;
+	private static final int ROW_STEP = 20;
+	private static final int COLUMN_STEP = 100;
+	
+	
+	public void run() {
+		
+	}
 }
