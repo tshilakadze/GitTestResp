20eaffdd04cb5c1d7f041af45b875d7d9a44ac7c
S <s>
2024-01-23 15:26:49 +0400

    {"types":"C","runs":14}

--- testAgain.java
+++ testAgain.java
@@ -8,12 +8,7 @@ import acm.program.ConsoleProgram;
 public class testAgain extends ConsoleProgram {
 //	private static final String SENTINEL = "";
 	public void run() {
-		ArrayList<String> bla = new ArrayList<String>();
-		bla.add("bla bla bla");
-		bla.add("nfncimeci");
-		println(bla.toString());
-		bla.remove("bla  bla");
-		println(bla.toString());
+		
 	}
 	
 }
