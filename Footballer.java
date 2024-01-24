8aca3f9f85aeb740b05e52758855974102a6f8c1
S <s>
2024-01-24 15:02:39 +0400

    {"types":"C","runs":1}

--- Footballer.java
+++ Footballer.java
@@ -1,4 +1,7 @@
+import java.util.Iterator;
 
 public interface Footballer {
-
+	public void addFootballPlayer(String club, String player);
+	public Iterator<String> getClubs(String player);
+	public Iterator<String> getPlayers(String club, int n);
 }
