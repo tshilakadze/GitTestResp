c49ac94087dabdbfef3df2b92b2c60f53d08d4f7
S <s>
2024-01-24 15:03:44 +0400

    {"types":"C","runs":1}

--- FinalLastYear_5.java
+++ FinalLastYear_5.java
@@ -1,4 +1,22 @@
+import java.util.Iterator;
 
-public class FinalLastYear_5 {
+public class FinalLastYear_5 implements Footballer{
+
+	@Override
+	public void addFootballPlayer(String club, String player) {
+		
+	}
+
+	@Override
+	public Iterator<String> getClubs(String player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<String> getPlayers(String club, int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
