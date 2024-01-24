23234fa16d2011f94172c3545af8b495d3e1de26
S <s>
2024-01-24 16:02:44 +0400

    {"types":"C","runs":1}

--- FootballTester.java
+++ FootballTester.java
@@ -1,4 +1,43 @@
+import java.util.Iterator;
 
-public class FootballTester {
+import acm.program.ConsoleProgram;
 
+public class FootballTester extends ConsoleProgram {
+	public void run() {
+		FinalLastYear_5 football = new FinalLastYear_5();
+		football.addFootballPlayer("dinamo", "messi");
+		football.addFootballPlayer("real", "messi");
+		football.addFootballPlayer("olimpi", "messi");
+		football.addFootballPlayer("barcellona", "messi");
+		
+		football.addFootballPlayer("real", "mari");
+		football.addFootballPlayer("olimpi", "mari");
+		football.addFootballPlayer("goodClub", "mari");
+		football.addFootballPlayer("goodClub", "nino");
+		football.addFootballPlayer("goodClub2", "goodPlayer");
+		
+		
+		printIteratorElems(football.getClubs("messi"));
+		printIteratorElems(football.getClubs("mari"));
+		printIteratorElems(football.getClubs("nino"));
+
+		printIteratorElems(football.getPlayers("dinamo", 3)); // arguments dont matter
+		
+
+	}
+
+	private void printIteratorElems(Iterator<String> it) {
+		if (it == null) {
+			println("null");
+		} else {
+			if (!it.hasNext()) {
+				println("empty");
+			} else {
+				while (it.hasNext()) {
+					print(it.next() + ", ");
+				}
+				println();
+			}
+		}
+	}
 }
