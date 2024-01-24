fab84ca88a3e0eb6430c1e8afc4ab32296d9a116
S <s>
2024-01-24 15:10:01 +0400

    {"types":"C","runs":1}

--- FinalLastYear_5.java
+++ FinalLastYear_5.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class FinalLastYear_5 implements Footballer{
-
+	private Map<String, String> clubPlayer = new HashMap<String, String>();
+	private Map<String, String> playerClub = new HashMap<String, String>();
 	@Override
 	public void addFootballPlayer(String club, String player) {
 		
