39b1e81a3acc1ef75dda5320b6a18c4cb931960d
S <s>
2024-01-29 21:45:14 +0400

    {"types":"C","runs":7}

--- Problem68.java
+++ Problem68.java
@@ -1,7 +1,10 @@
+import javax.swing.JTextField;
+
 import acm.program.*;
 
 public class Problem68 extends GraphicsProgram {
 	public void run() {
-		
+		JTextField message = new JTextField(20);
+		add(message, SOUTH);
 	}
 }
