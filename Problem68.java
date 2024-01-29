fc6cbb4121ff80893cf0850b734a50f48c75be1c
S <s>
2024-01-29 22:03:08 +0400

    {"types":"C","runs":17}

--- Problem68.java
+++ Problem68.java
@@ -23,7 +23,7 @@ public class Problem68 extends GraphicsProgram {
 		if(e.getActionCommand().equals("Enter") || e.getSource() == message){
 			GLabel mas = new GLabel(message.getText());
 			mas.setFont("Dialog-16-bold");
-			add(mas, 20, 20);
+			add(mas, 0, 20);
 			message.setText("");
 		}
 	}
