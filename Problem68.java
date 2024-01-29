ddf9bd97981a63036d50e4c36aa7cebb4ff4a6af
S <s>
2024-01-30 01:09:01 +0400

    {"types":"C","runs":20}

--- Problem68.java
+++ Problem68.java
@@ -22,15 +22,8 @@ public class Problem68 extends GraphicsProgram {
 	
 	public void actionPerformed(ActionEvent e){
 		if(e.getActionCommand().equals("Enter") || e.getSource() == message){
-			double y = 0;
-			ArrayList<String> list = new ArrayList<String>();
-			list.add(message.getText());
 			GLabel mas = new GLabel(message.getText());
 			mas.setFont("Dialog-16-bold");
-			for(int i = 0; i < list.size(); i++){
-				y = y + mas.getHeight() * (i + 1);
-				add(mas, 0, y);
-			}
 			message.setText("");
 		}
 	}
