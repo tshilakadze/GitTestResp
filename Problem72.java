58995f8f049cb04f9c391dbc9bf5d2f152819ec4
S <s>
2024-01-30 17:56:20 +0400

    {"types":"C","runs":10}

--- Problem72.java
+++ Problem72.java
@@ -52,6 +52,8 @@ public class Problem72 extends GraphicsProgram {
 	
 	public void actionListeners(ActionEvent e){
 		if(e.getActionCommand().equals("Add Teach") && !teach.getText().equals("")){
+			GLabel a = new GLabel("Fuck eclipse");
+			add(a, 100, 100);
 			school.addTeacher(teach.getText());
 			pupil.setText("");
 			subj.setText("");
