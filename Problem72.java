9daf182c5ddb4a422d57993c69e8d984fdbaafba
S <s>
2024-01-30 18:10:41 +0400

    {"types":"C","runs":23}

--- Problem72.java
+++ Problem72.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
-public class Problem72 extends Program {
+public class Problem72 extends GraphicsProgram {
 	
 	private static final int RESULT_X = 20;
 	private static final int LINE_H = 20;
