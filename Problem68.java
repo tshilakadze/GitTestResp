6e3ea612c066d44b33ae4bde54f35fc63848275c
S <s>
2024-01-30 14:45:20 +0400

    {"types":"C","runs":3}

--- Problem68.java
+++ Problem68.java
@@ -37,6 +37,7 @@ public class Problem68 extends GraphicsProgram {
 			compMess.add(mes, 2, y);
 			y += 20;
 			GLabel bla = new GLabel(compMess.getHeight() + "");
+			add(bla, 2, y - 20);
 			message.setText("");
 		}
 	}
