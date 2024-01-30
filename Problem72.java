080c58a56ed2dd0948112f371c726e2d61758633
S <s>
2024-01-30 15:43:09 +0400

    {"types":"C","runs":23}

--- Problem72.java
+++ Problem72.java
@@ -1,4 +1,12 @@
+import acm.program.GraphicsProgram;
 
-public class Problem72 {
+public class Problem72 extends GraphicsProgram {
+	
+	private static final int RESULT_X = 20;
+	private static final int LINE_H = 20;
+	
+	public void init() {
+		
+	}
 
 }
