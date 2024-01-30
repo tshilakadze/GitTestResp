2ad2d39df7b75510c8b543bb242b4c683d4dfad8
S <s>
2024-01-30 15:45:46 +0400

    {"types":"C","runs":25}

--- testAgain.java
+++ testAgain.java
@@ -1,107 +1,125 @@
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
-import javax.swing.*;
+public class testAgain {
+	private HashMap<String, HashSet<String>> teacherSubject;
+	private HashMap<String, HashSet<String>> subjectTeacher;
+	private HashMap<String, HashSet<String>> pupilSubject;
+	private HashMap<String, HashSet<String>> subjectPupil;
 
-import acm.graphics.*;
-import acm.program.GraphicsProgram;
-
-
-public class testAgain extends GraphicsProgram {
-	private static final int COLUMN_START_X = 10;
-	private static final int COLUMN_START_Y = 20;
-	private static final int ROW_STEP = 20;
-	private static final int COLUMN_STEP = 100;
-
-	private JTextField colField;
-	private JTextField rowField;
-	private JTextField filterField;
-	private JButton colButton;
-	private JButton rowButton;
-	private JButton filterButton;
-
-	private ArrayList<String> columnNames;
-	private ArrayList<ArrayList<String>> rows;
-
-	public void init() {
-		colField = new JTextField(10);
-		rowField = new JTextField(10);
-		filterField = new JTextField(10);
-		colButton = new JButton("add column");
-		rowButton = new JButton("add row");
-		filterButton = new JButton("filter");
-
-		add(colField, SOUTH);
-		add(colButton, SOUTH);
-		add(rowField, SOUTH);
-		add(rowButton, SOUTH);
-		add(filterField, SOUTH);
-		add(filterButton, SOUTH);
-
-		addActionListeners();
-
-		columnNames = new ArrayList<>();
-		rows = new ArrayList<>();
+//	კონსტრუქტორს არაფერი არ გადაეცემა. აქ შეგიძლიათ ინიციალიზაცია გაუკეთოთ
+//	თქვენთვის საჭირო ცვლადებს.
+	public School() {
+		teacherSubject = new HashMap<>();
+		subjectTeacher = new HashMap<>();
+		pupilSubject = new HashMap<>();
+		subjectPupil = new HashMap<>();
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == colButton) {
-			addColumn(colField.getText());
-		} else if (e.getSource() == rowButton) {
-			addRow(rowField.getText());
-		} else if (e.getSource() == filterButton) {
-			filterRows(filterField.getText());
+	private void printsForTesting() {
+		System.out.println("-------");
+		System.out.println("teacherSubject: " + teacherSubject);
+		System.out.println("subjectTeacher: " + subjectTeacher);
+		System.out.println("pupilSubject: " + pupilSubject);
+		System.out.println("subjectPupil: " + subjectPupil);
+	}
+
+//	addTeacher მეთოდის საშუალებით შეგიძლიათ სკოლას დაამატოთ ახალი
+//	მასწავლებელი. მეთოდს გადაეცემა მასწავლებლის სახელი. შეგიძლიათ ჩათვალოთ,
+//	რომ მასწავლებლის სახელი უნიკალურია.
+	public void addTeacher(String teacher) {
+		teacherSubject.putIfAbsent(teacher, new HashSet<String>());
+
+		// TIP: you can use System.out.println() to print your structures for testing
+		printsForTesting();
+	}
+
+//	addSubject მეთოდის საშუალებით შეგიძლიათ მასწავლებელს დაუმატოთ საგანი.
+//	მეთოდს გადაეცემა მასწავლებლის სახელი და საგნის სახელი. ჩათვალეთ, რომ საგნის
+//	სახელი უნიკალურია. ერთი და იგივე მასწავლებელი შეიძლება რამდენიმე საგანს
+//	კითხულობდეს, ასევე ერთსა და იმავე საგანს შეიძლება რამდენიმე მასწავლებელი
+//	კითხულობდეს ერთდროულად.
+//	თუკი teacher სახელის მქონე მასწავლებელი არ არის აქამდე დამატებული, მაშინ
+//	მეთოდმა არაფერი არ უნდა გააკეთოს.
+	public void addSubject(String teacher, String subject) {
+		if (!teacherSubject.containsKey(teacher)) {
+			return;
 		}
+		teacherSubject.get(teacher).add(subject);
+
+		subjectTeacher.putIfAbsent(subject, new HashSet<String>());
+		subjectTeacher.get(subject).add(teacher);
+
+		// TIP: you can use System.out.println() to print your structures for testing
+		printsForTesting();
 	}
 
-	private void filterRows(String text) {
-		showGrid(text);
-	}
-
-	private void addRow(String text) {
-		StringTokenizer tokenizer = new StringTokenizer(text, ";");
-		ArrayList<String> newRow = new ArrayList<>();
-		while (tokenizer.hasMoreTokens()) {
-			newRow.add(tokenizer.nextToken());
+//	addPupil მეთოდის საშუალებით შეგიძლიათ საგანზე დაამატოთ მოსწავლე. ერთი და
+//	იგივე მოსწავლე შეიძლება ერთ ან რამდენიმე საგანს სწავლობდეს.
+	public void addPupil(String pupil, String subject) {
+		if (!subjectTeacher.containsKey(subject)) {
+			return;
 		}
-		rows.add(newRow);
 
-		showGrid(null);
+		subjectPupil.putIfAbsent(subject, new HashSet<String>());
+		subjectPupil.get(subject).add(pupil);
+
+		pupilSubject.putIfAbsent(pupil, new HashSet<String>());
+		pupilSubject.get(pupil).add(subject);
+
+		// TIP: you can use System.out.println() to print your structures for testing
+		printsForTesting();
 	}
 
-	private void showGrid(String filterWrd) {
-		displayColumnNames();
-		int y = COLUMN_START_Y;
-		for (ArrayList<String> row : rows) {
-			if (filterWrd == null || row.contains(filterWrd)) {
-				y += ROW_STEP;
-				displayRow(row, y);
+//	getTeachers მეთოდს გადაეცემა მოსწავლის სახელი და მან უნდა დააბრუნოს ამ
+//	მოსწავლის ყველა მასწავლებელზე იტერატორი. ანუ მხოლოდ იმ მასწავლებლების
+//	სახელები, რომლებიც ასწავლიან იმ საგნებს, რომლებზეც მოსწავლეა
+//	დამატებული(სწავლობს). თუკი pupil სახელის მოსწავლე არ გვყავს მაშინ მეთოდმა
+//	უნდა დააბრუნოს null.
+	public Iterator<String> getTeachers(String pupil) {
+		if (!pupilSubject.containsKey(pupil)) {
+			return null;
+		}
+		HashSet<String> teachers = new HashSet<>();
+		for (String subject : pupilSubject.get(pupil)) {
+			if (subjectTeacher.containsKey(subject)) {
+				teachers.addAll(subjectTeacher.get(subject)); // TODO test on addAll(null);
 			}
 		}
+
+		return teachers.iterator();
 	}
 
-	private void addColumn(String text) {
-		columnNames.add(text);
-		displayColumnNames();
-	}
-
-	private void displayColumnNames() {
-		removeAll();
-		displayRow(columnNames, COLUMN_START_Y);
-		GLine line = new GLine(COLUMN_START_X, COLUMN_START_Y, COLUMN_START_X + COLUMN_STEP * columnNames.size(),
-				COLUMN_START_Y);
-		add(line);
-	}
-
-	private void displayRow(ArrayList<String> row, int y) {
-		int x = COLUMN_START_X;
-		for (int i = 0; i < row.size(); i++) {
-			GLabel label = new GLabel(row.get(i));
-			add(label, x, y);
-			x += COLUMN_STEP;
+//	getPupils მეთოდს გადაეცემა მასწავლებლის სახელი და მან უნდა დააბრუნოს ამ
+//	მასწავლებლის ყველა სტუდენტზე იტერატორი. ანუ მხოლოდ იმ სტუდენტების
+//	სახელები, რომლებიც მის რომელიმე საგანს სწავლობენ. თუკი teacher სახელის მქონე
+//	ლექტორი არ გვყავს მაშინ მეთოდმა უნდა დააბრუნოს null.
+	public Iterator<String> getPupils(String teacher) {
+		if (!teacherSubject.containsKey(teacher)) {
+			return null;
 		}
+		HashSet<String> pupils = new HashSet<>();
+		for (String subject : teacherSubject.get(teacher)) {
+			if (subjectPupil.containsKey(subject)) {
+				pupils.addAll(subjectPupil.get(subject));
+			}
+		}
+		return pupils.iterator();
+	}
+
+//	მეთოდმა უნდა წაშალოს მასწავლებლის შესახებ ყველა ინფორმაცია. ამ მეთოდის
+//	გამოძახების, შემდეგ getTeachers მეთოდმა არ უნდა დააბრუნოს teacher სახელი არც
+//	ერთი სტუდენტისთვის.
+	public void removeTeacher(String teacher) {
+		teacherSubject.remove(teacher);
+		for(String subject: subjectTeacher.keySet()) {
+			HashSet<String> teachers = subjectTeacher.get(subject);
+			teachers.remove(teacher);
+		}
+		
+		// TIP: you can use System.out.println() to print your structures for testing
+		printsForTesting();
 	}
 
 }
